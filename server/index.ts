0351bf6d34ca64a0142d25d59a0f525b1c7d1d87
cody-daigle <cody.h.daigle@gmail.com>
2024-06-01 15:53:32 -0500

    Start up structure of server

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,23 @@
+const path = require('path');
+const express = require('express');
+require('dotenv').config({path: '../.env'}) //???
 
+const route = require('./routes/index.ts')
+const {PORT = 3000} = process.env;
 
-const hi = (name: string):void => {
-console.log(`Hello${name}`)
-};
+const CLIENT = path.resolve(__dirname, 'client/index.html')
 
-hi('patrick');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(CLIENT))
+app.use('/api', route);
+
+app.listen(PORT, () => {
+  console.info(`http://localhost:${PORT} \n\n http://127.0.0.1:${PORT}`)
+})
+
+module.exports = {
+  app,
+}
