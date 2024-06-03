272f2eeddc5aaef538ac7cdbc46453f1f1ceadcb
mike <sammartino.mike@gmail.com>
2024-06-03 17:59:24 -0400

    starting to implement route with require syntax

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,10 +2,13 @@ const { Router } = require('express')
 /*
 * all the routes for features
 */
+const messages = require('./messages.ts')
 
 const api = Router();
+const routeHandler = Router();
 
 // api.use(/*'/something', model? */)
+api.all('/messages', messages);
 
 // export default api
 module.exports = api
--- server/routes/messages.ts
+++ server/routes/messages.ts
@@ -1,2 +1,8 @@
+const express = require('express');
 const messageRoutes = express.Router();
 
+messageRoutes.get('/', (req, res) => {
+  res.send('Reached messages GET route successfully');
+});
+
+module.exports = messageRoutes;
