548eb110f283f64837292eacc6908b64d1658f20
Hackman78 <pathenry231@gmail.com>
2024-06-03 16:34:53 -0500

    link not fully working yet

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,7 +1,13 @@
 import React, { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
 
 const App = (): ReactElement => {
-  return <>Teachers aka Users</>;
+  return (
+    <>
+      <Link to='/dashboard'>Dashboard</Link>
+      <h1>Teachers aka Users</h1>
+    </>
+  );
 };
 
 export default App;
