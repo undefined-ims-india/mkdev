ac2573d9af0ea237147ffc83ec1b8fca6ffdc6ba
cody-daigle <cody.h.daigle@gmail.com>
2024-06-03 15:01:53 -0500

    Authentication routes placed in index.ts.

--- server/index.ts
+++ server/index.ts
@@ -5,16 +5,15 @@ const express = require('express');
 const { auth, requiresAuth } = require('express-openid-connect');
 const CLIENT = path.resolve(__dirname, 'client/index.html');
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, PORT = 3000 } = process.env;
-// const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, BASE_URL, PORT, NODE_ENV } = process.env;
-console.log(GOOGLE_CLIENT_SECRET);
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(CLIENT));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /*** AUTH ***/
+
 const config = {
   authRequired: false,
   auth0Logout: true,
@@ -26,25 +25,18 @@ const config = {
 
 app.use(auth(config));
 
-app.get('/', (req: any, res: any) => {
-  res.send(req.oidc.isAuthenticated()); // * Returns 'false'
-  // res.render('index', {
-  //   title: '0Auth',
-  //   isAuthenticated: req.oidc.isAuthenticated(),
-  // });
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated()); // * Returns 'false' at the moment
 });
 
-app.use((req: any, res: any) => {
+app.use((req, res) => {
   res.locals.user = req.oidc.user;
 });
 
-app.get('/profile', requiresAuth(), (req: any, res: any) => {
+app.get('/profile', requiresAuth(), (req, res) => {
   res.send(JSON.stringify(req.oidc.user, null, 2));
-  // res.render('profile', {
-  //   userProfile: JSON.stringify(req.oidc.user, null, 2),
-  //   title: 'Profile page',
-  // });
 });
+/*** AUTH ***/
 
 app.listen(PORT, () => {
   console.info(`http://localhost:${PORT} \n\n http://127.0.0.1:${PORT}`);
