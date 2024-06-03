449af835ca73a36620c044e0ea07ad1b6883e765
AlexPHebert2000 <aphebert9994@gmail.com>
2024-06-03 17:33:57 -0500

    ADD post router file with test

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,10 +2,12 @@ const { Router } = require('express')
 /*
 * all the routes for features
 */
+const postsRouter = require('./routers/posts.ts')
 
 const api = Router();
 
 // api.use(/*'/something', model? */)
+api.use('/posts', postsRouter);
 
 // export default api
 module.exports = api
