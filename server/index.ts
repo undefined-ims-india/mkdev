7741b7d29ebbd8ffc8c5687c3bb9cdf79acb8f9e
cody-daigle <cody.h.daigle@gmail.com>
2024-06-03 14:40:26 -0500

    Moved Auth to index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,23 +1,51 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
-require('dotenv').config({path: '../.env'}) //???
 
-const route = require('./routes/index.ts')
-const {PORT = 3000} = process.env;
-
-const CLIENT = path.resolve(__dirname, '..', 'dist')
+const { auth, requiresAuth } = require('express-openid-connect');
+const CLIENT = path.resolve(__dirname, 'client/index.html');
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, PORT = 3000 } = process.env;
+// const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, BASE_URL, PORT, NODE_ENV } = process.env;
+console.log(GOOGLE_CLIENT_SECRET);
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(CLIENT));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(CLIENT))
-app.use('/api', route);
+
+/*** AUTH ***/
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: `http://localhost:${PORT}`,
+  clientID: GOOGLE_CLIENT_ID,
+  secret: GOOGLE_CLIENT_SECRET,
+  issuerBaseURL: `https://dev-uatvgw7p2cq7mmm0.us.auth0.com`,
+};
+
+app.use(auth(config));
+
+app.get('/', (req: any, res: any) => {
+  res.send(req.oidc.isAuthenticated()); // * Returns 'false'
+  // res.render('index', {
+  //   title: '0Auth',
+  //   isAuthenticated: req.oidc.isAuthenticated(),
+  // });
+});
+
+app.use((req: any, res: any) => {
+  res.locals.user = req.oidc.user;
+});
+
+app.get('/profile', requiresAuth(), (req: any, res: any) => {
+  res.send(JSON.stringify(req.oidc.user, null, 2));
+  // res.render('profile', {
+  //   userProfile: JSON.stringify(req.oidc.user, null, 2),
+  //   title: 'Profile page',
+  // });
+});
 
 app.listen(PORT, () => {
-  console.info(`\nhttp://localhost:${PORT}\nhttp://127.0.0.1:${PORT}`)
-})
-
-module.exports = {
-  app,
-}
+  console.info(`http://localhost:${PORT} \n\n http://127.0.0.1:${PORT}`);
+});
