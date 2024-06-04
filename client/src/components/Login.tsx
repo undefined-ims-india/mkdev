3f2de7138a3333f96dd2605e1c5f5c105af95672
cody-daigle <cody.h.daigle@gmail.com>
2024-06-04 13:40:01 -0500

    Add / Login page for user to login through authentication/user:password

---
+++ client/src/components/Login.tsx
@@ -0,0 +1,20 @@
+
+
+import React, { ReactElement } from 'react';
+import Nav from './Nav'
+
+const Login = (): ReactElement => {
+  return (
+    <>
+ <h2>Login page</h2>
+ <h4> Will be adding the auth routing here </h4>
+ <br/>
+ <h4>Here are the routes to navigate for now</h4>
+ <>
+ <Nav />
+ </>
+    </>
+  );
+};
+
+export default Login;
