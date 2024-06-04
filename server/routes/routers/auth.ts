417f2fa45201239f586cf3e31c172923e10ec633
cody-daigle <cody.h.daigle@gmail.com>
2024-06-04 15:21:44 -0500

    Add / auth0 doc recommendation for auth router

--- server/routes/routers/auth.ts
+++ server/routes/routers/auth.ts
@@ -3,12 +3,23 @@ import { requiresAuth } from 'express-openid-connect';
 
 const auth = Router();
 
+// auth.get('/', (req, res) => {
+//   res.render('index', {
+//     title: 'Auth0 Webapp sample Nodejs',
+//     isAuthenticated: req.oidc.isAuthenticated(),
+//   });
+// });
+
+// req.isAuthenticated is provided from the auth router
 auth.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated());
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
 auth.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user, null, 2));
+  res.render('profile', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Profile page',
+  });
 });
 
 export default auth;
