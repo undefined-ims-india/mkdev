fe2adf6da531e4d9160b85d6af4d251306127905
mike <sammartino.mike@gmail.com>
2024-06-04 12:44:23 -0400

    message GET successful, message POST generated type error

--- server/routes/messages.ts
+++ server/routes/messages.ts
@@ -4,12 +4,33 @@ import { PrismaClient } from '@prisma/client';
 const messages = Router();
 const prisma = new PrismaClient();
 
+// type message = {
+//   body: string;
+// }
+
+// Messages routes
+
 messages.get('/', async (req, res) => {
   const allMessages = await prisma.messages.findMany();
   res.status(200).send(allMessages);
 });
 
-messages.post('/', (req: Request, res: Response) => {
+messages.post('/', async (req: Request, res: Response) => {
+  const { body, sender, conversation } = req.body.message;
+
+  // query database for sender user id,
+  // query database for conversation id?
+  // then create message based on those ids (bc they should have
+  //    correct type like that?)
+
+  await prisma.messages.create({
+    data: {
+      body: body,
+      sender: sender,
+      conversation: conversation,
+    }
+  })
+
   res.sendStatus(201);
 })
 
