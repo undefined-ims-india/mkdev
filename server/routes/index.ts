60149c47e09571698ca9e17ff11fab3b7a8b5b9c
mike <sammartino.mike@gmail.com>
2024-06-04 12:44:06 -0400

    add conversations route

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -4,6 +4,7 @@ import { Router } from 'express';
 */
 
 import messages from './messages';
+import conversations from './conversations';
 const postsRouter = require('./routers/posts.ts')
 const usersRouter = require('./routers/users.ts')
 
@@ -13,6 +14,7 @@ const api = Router();
 api.use('/posts', postsRouter);
 api.use('/users', usersRouter);
 api.use('/messages', messages);
+api.use('/conversations', conversations);
 
 // export default api
 export default api;
