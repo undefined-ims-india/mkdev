d5b023edb76a1d2e4602ef106c2931ffb204da8c
cody-daigle <cody.h.daigle@gmail.com>
2024-06-04 15:22:24 -0500

    Add / added auth config

--- server/index.ts
+++ server/index.ts
@@ -1,39 +1,35 @@
 import path from 'path';
 import dotEnv from 'dotenv';
-dotEnv.config();
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { auth } from 'express-openid-connect';
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, PORT = 3000 } = process.env;
-
 import routes from './routes';
 
+const { CLIENT_ID, SECRET, PORT = 3000 } = process.env;
+
+dotEnv.config();
 const CLIENT = path.resolve(__dirname, '..', 'dist');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static(CLIENT));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(CLIENT));
 app.use('/api', routes);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(CLIENT, 'index.html'));
-});
 
 const config = {
   authRequired: false,
   auth0Logout: true,
   baseURL: `http://localhost:${PORT}`,
-  clientID: GOOGLE_CLIENT_ID,
-  secret: GOOGLE_CLIENT_SECRET,
+  clientID: CLIENT_ID,
   issuerBaseURL: 'https://dev-uatvgw7p2cq7mmm0.us.auth0.com',
+  secret: SECRET,
 };
 
+// auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
-app.use((req, res) => {
-  res.locals.user = req.oidc.user;
+app.get('*', (req: Request, res: Response) => {
+  res.sendFile(path.join(CLIENT, 'index.html'));
 });
 
 app.listen(PORT, () => {
