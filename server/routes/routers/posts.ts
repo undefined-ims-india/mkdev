4eca874750447375b04e4fe0dbece8491f627c95
AlexPHebert2000 <aphebert9994@gmail.com>
2024-06-04 13:03:19 -0500

    ADD request handling for POST /api/posts

--- server/routes/routers/posts.ts
+++ server/routes/routers/posts.ts
@@ -2,11 +2,26 @@ const { Router } = require('express');
 const {PrismaClient} = require('@prisma/client');
 
 const posts = Router();
+const prisma = new PrismaClient();
+
+const USER_ID = 3;
 
 posts.post('/', (req:any, res:any) => {
-  const { newPost } : {newPost: {body: string}} = req.body;
-  console.log(newPost);
-  res.end();
-})
+  const { newPost } : {newPost: {title: string, body: string}} = req.body;
+  prisma.post.create({
+    data: {...newPost, author: {connect: {id: USER_ID}}}
+  })
+    .then((post: any) => {
+      console.log(post);
+      res.sendStatus(201)
+    })
+    .catch((err: string) => {
+      console.error(err);
+      res.sendStatus(500)
+    })
+    .finally(async () => {
+      await prisma.$disconnect()
+    });
+});
 
 module.exports = posts
