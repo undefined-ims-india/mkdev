905fd047290b7ab71ff2ef19bdfd32ecd401c089
cody-daigle <cody.h.daigle@gmail.com>
2024-06-04 13:39:11 -0500

    Add / Profile component for user's personal profile

---
+++ client/src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react';
+import Nav from './Nav';
+
+const Profile = (): ReactElement => {
+  return (
+    <>
+    <p> Welcome to the profile page</p>
+  <Nav/>
+    </>
+  )
+};
+
+export default Profile;
