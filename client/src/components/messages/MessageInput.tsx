a1a062bfd813077e47824efa1a3e1438fa336c80
mike <sammartino.mike@gmail.com>
2024-06-05 17:55:54 -0400

    add basic structure for all components and component tree

---
+++ client/src/components/messages/MessageInput.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+const MessageInput = (): ReactElement => {
+  return (
+    <>
+      <h3>The MessageInput component will be here</h3>
+    </>
+  );
+}
+
+export default MessageInput;
--- client/src/components/messages/Messages.tsx
+++ client/src/components/messages/Messages.tsx
@@ -1,8 +1,14 @@
 import React, { ReactElement } from 'react';
+import Conversations from './Conversations';
+import ConversationView from './ConversationView';
 
 const Messages = (): ReactElement => {
   return (
-    <h1>The messages will be here</h1>
+    <>
+      <h1>The messages will be here</h1>
+      <Conversations />
+      <ConversationView />
+    </>
   );
 }
 
