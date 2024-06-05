3e1276b8ffe2cc12db4e75e5777715b5c6fd4d23
cody-daigle <cody.h.daigle@gmail.com>
2024-06-05 11:16:30 -0500

    Converted requires to imports

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,18 +1,14 @@
 import { Router } from 'express';
-/*
-* all the routes for features
-*/
-
 import messages from './messages';
 import conversations from './conversations';
-const postsRouter = require('./routers/posts.ts')
-const usersRouter = require('./routers/users.ts')
+import posts from './routers/posts';
+import users from './routers/users';
 
 const api = Router();
 
 // api.use(/*'/something', model? */)
-api.use('/posts', postsRouter);
-api.use('/users', usersRouter);
+api.use('/posts', posts);
+api.use('/users', users);
 api.use('/messages', messages);
 api.use('/conversations', conversations);
 
