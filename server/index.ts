eb7e166feddbfee5286ca3aec660b1fb527646cc
mike <sammartino.mike@gmail.com>
2024-06-05 15:53:21 -0400

    add websocket server connection on port 4000 and confirmation console.log

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,8 @@ import path from 'path';
 import dotEnv from 'dotenv';
 dotEnv.config({ path: '../.env' }); //???
 import express, { Request, Response } from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 
 import routes from './routes';
 
@@ -11,16 +13,32 @@ const { PORT = 3000 } = process.env;
 const CLIENT = path.resolve(__dirname, '..', 'dist');
 
 const app = express();
+const socket = express();
+const server = createServer(socket);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(CLIENT));
 app.use('/api', routes);
 
-app.get('*', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.sendFile(path.join(CLIENT, 'index.html'));
 });
+
+// socket handling ----------------------------------------- //
+io.on('connection', (socket) => {
+  console.log('A user has connected');
+});
+// socket handling ----------------------------------------- //
+
+// websocket server
+server.listen(4000);
 
 app.listen(PORT, () => {
   console.info(`\nhttp://localhost:${PORT}\nhttp://127.0.0.1:${PORT}`);
 });
+
+export default app;
