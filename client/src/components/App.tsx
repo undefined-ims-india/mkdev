051ffa484769523bb5d5cdbfaafae361e25a5b7e
Alex <aphebert2000@gmail.com>
2024-06-04 21:23:43 -0500

    ADD basic post creation

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,21 @@
 import React, { ReactElement } from 'react';
-import { Link } from 'react-router-dom';
 import '../styling/index.css'
+import { Link } from 'react-router-dom';
+import { Button } from '@headlessui/react';
 
 const App = (): ReactElement => {
   return (
     <>
-      <Link to='/dashboard'>Dashboard</Link>
+      <Link to='/dashboard'>
+          <Button className="bg-sky-900 backdrop-blur-sm text-white text-m">
+            Dashboard
+          </Button>
+        </Link>
+      <Link to='/create-post'>
+          <Button className="bg-sky-900 backdrop-blur-sm text-white text-m">
+            Create Post
+          </Button>
+        </Link>
       <h1>Teachers aka Users</h1>
       <h1 className="text-3xl font-bold underline">
       Hello world!
