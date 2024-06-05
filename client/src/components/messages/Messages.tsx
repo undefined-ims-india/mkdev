6c3b6c65eb6fdf89cb4f181d0ec03387ded03d4a
mike <sammartino.mike@gmail.com>
2024-06-05 18:13:11 -0400

    add socket.io client

--- client/src/components/messages/Messages.tsx
+++ client/src/components/messages/Messages.tsx
@@ -2,6 +2,10 @@ import React, { ReactElement } from 'react';
 import Conversations from './Conversations';
 import ConversationView from './ConversationView';
 
+// this is where I want the socketio client to go instead of on index.html
+import io from 'socket.io-client';
+const socket = io('http://localhost:4000');
+
 const Messages = (): ReactElement => {
   return (
     <>
