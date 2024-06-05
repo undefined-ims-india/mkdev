14aca1b503a91458eaaa9e845b092aee6f0a5c8c
cody-daigle <cody.h.daigle@gmail.com>
2024-06-05 11:12:12 -0500

    Add / posts and user state for profile page

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,7 +1,11 @@
 import React, { ReactElement, useState, useEffect } from 'react';
+import axios from 'axios';
 import Nav from './Nav';
 
 const Profile = (): ReactElement => {
+  const [user, setUser] = useState();
+  const [posts, setPosts] = useState([]);
+
   return (
     <div>
       <h1> Profile </h1>
