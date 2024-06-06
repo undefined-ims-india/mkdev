e651e01147a45f6e25d083092792f4d2f4f559c4
mike <sammartino.mike@gmail.com>
2024-06-05 20:36:52 -0400

    handle text input and send to socket server

--- client/src/components/messages/MessageInput.tsx
+++ client/src/components/messages/MessageInput.tsx
@@ -1,10 +1,37 @@
-import React, { ReactElement } from 'react';
+import React, { useState, ReactElement } from 'react';
+import io from 'socket.io-client';
+const socket = io('http://localhost:4000');
 
 const MessageInput = (): ReactElement => {
+  const [text, setText] = useState('');
+
+  const handleText = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setText(e.target.value);
+    console.log('text input: ', text);
+  }
+
+  const sendMessage = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+    e.preventDefault();
+    console.log('sent text', text);
+    socket.emit('message', text);
+    // if (input.value) {
+    //   // broadcast the message to all the clients
+    //   socket.emit('chat message', input.value);
+    //   // reset the input value to empty string
+    //   input.value = '';
+    // }
+    setText('');
+  }
+
   return (
-    <>
-      <h3>The MessageInput component will be here</h3>
-    </>
+    <div>
+      <form>
+        <input value={ text } onChange={ handleText } className="shadow appearance-none border-2 border-black-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"/>
+        <button onClick={ sendMessage } className="bg-gray-300 p-1.5 rounded">
+          Send
+        </button>
+      </form>
+    </div>
   );
 }
 
