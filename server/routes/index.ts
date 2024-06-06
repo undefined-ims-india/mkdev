7fb2fa1be489da420613d03a8230daf1f84bbc0a
cody-daigle <cody.h.daigle@gmail.com>
2024-06-06 17:37:10 -0500

    Removed unused routes

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -4,14 +4,12 @@ import messages from './messages';
 import conversations from './conversations';
 import posts from './routers/posts';
 import users from './routers/users';
-// const postsRouter = require('./routers/posts.ts');
-// const usersRouter = require('./routers/users.ts');
 
 const api = Router();
+
 api.use('/posts', posts);
 api.use('/users', users);
-// api.use('/posts', postsRouter);
-// api.use('/users', usersRouter);
+
 api.use('/messages', messages);
 api.use('/conversations', conversations);
 
