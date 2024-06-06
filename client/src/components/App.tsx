a82dd70352e69fb7a6f91cc5e8af1ed98827c69d
cody-daigle <cody.h.daigle@gmail.com>
2024-06-06 12:15:56 -0500

    Added login button

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,20 +1,19 @@
 import React, { ReactElement } from 'react';
-import '../styling/index.css'
+import '../styling/index.css';
 import { Link } from 'react-router-dom';
 
 const App = (): ReactElement => {
   return (
     <>
       <Link to='/dashboard'>
-          <button>
-            Dashboard
-          </button>
-        </Link>
+        <button>Dashboard</button>
+      </Link>
       <Link to='/create-post'>
-          <button>
-            Create Post
-          </button>
-        </Link>
+        <button>Create Post</button>
+      </Link>
+      <Link to='/login'>
+        <button>Login</button>
+      </Link>
       <h1>Teachers aka Users</h1>
     </>
   );
