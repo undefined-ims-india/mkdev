059c15e336fdbfbe003ee2ef83b861b9c63379cf
cody-daigle <cody.h.daigle@gmail.com>
2024-06-06 23:29:21 -0500

    Profile page with Logged in User name and Picture displaying

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,17 +1,51 @@
-import React, { ReactElement, useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import Nav from './Nav';
+import { Card, CardContent, Typography } from '@material-ui/core';
+interface User {
+  name: string;
+  picture: string;
+}
 
-const Profile = (): ReactElement => {
-  const [user, setUser] = useState();
+const Profile = () => {
+  const [user, setUser] = useState<User | null>(null);
   const [posts, setPosts] = useState([]);
+  const userRef = useRef(user);
+
+  useEffect(() => {
+    axios
+      .get('/profile')
+      .then(({ data }) => {
+        setUser(data);
+        // console.log('user', data);
+      })
+      .catch((error) => {
+        console.error('Failed to get user:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(`/api/posts/${user.name}`)
+        .then(({ data }) => {
+          setPosts(data);
+          console.log("user's posts", data);
+        })
+        .catch((error) => {
+          console.error("Failed to get use's posts:", error);
+        });
+    }
+  }, [userRef]);
 
   return (
     <div>
-      <h1> Profile </h1>
-      <>
-        <Nav />
-      </>
+      <h1>Profile</h1>
+      {user && (
+        <div>
+          <h1>{user.name}</h1>
+          <img src={user.picture} alt={user.name} />
+        </div>
+      )}
     </div>
   );
 };
