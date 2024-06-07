bb3db9e9e133bdf7794dfc31d54ea731f0b1e3f5
mike <sammartino.mike@gmail.com>
2024-06-07 15:46:36 -0400

    retrieve messages from db

--- client/src/components/messages/ConversationView.tsx
+++ client/src/components/messages/ConversationView.tsx
@@ -1,15 +1,40 @@
 import React, { useState, ReactElement } from 'react';
+import axios from 'axios';
 import MessagesList from './MessagesList';
 import MessageInput from './MessageInput';
 import io from 'socket.io-client';
 
 const socket = io('http://localhost:4000');
 
+interface Message {
+    body: string;
+    senderId: number;
+}
+
 const ConversationView = (): ReactElement => {
 
-  const [allMsgs, setAllMsgs] = useState<string[]>([]);
+  const [allMsgs, setAllMsgs] = useState<Message[]>([]);
 
-  socket.on('message', (msg: string): void => {
+  const getAllMsgs = (): void => {
+    // send request to backend GET messages route
+    axios
+      .get('/api/messages')
+      .then(({ data }) => {
+        console.log('array of messages in db', data);
+        setAllMsgs(data);
+      })
+      .catch((err) => {
+        console.error('Failed to retrieve messages from db:\n', err);
+      })
+
+    // results from GET /api/messages are in array
+
+
+    // set allMsgs to result of query
+  }
+  // getAllMsgs();
+
+  socket.on('message', (msg: Message): void => {
     // console.log('msg emitted from server:', msg);
     // allMsgs.push(msg);
     // console.log('allMsgs from socket server', allMsgs);
