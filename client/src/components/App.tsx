7d1f0005a2728c6c14a383ef2f0c85fed1671c5b
cody-daigle <cody.h.daigle@gmail.com>
2024-06-08 13:01:09 -0500

    Added Nav component to App.tsx

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,31 +1,12 @@
 import React, { ReactElement } from 'react';
 import '../styling/index.css';
 import { Link } from 'react-router-dom';
+import Nav from './Nav';
 
 const App = (): ReactElement => {
   return (
     <>
-      <Link to='/'>
-        <h1>mkDev</h1>
-      </Link>
-      <Link to='/dashboard'>
-        <button>Dashboard</button>
-      </Link>
-      <Link to='/create-post'>
-        <button>Create Post</button>
-      </Link>
-      <Link to='/login'>
-        <button>Login</button>
-      </Link>
-      <Link to='/logout'>
-        <button>Logout</button>
-      </Link>
-      <Link to='/profile'>
-        <button>Profile</button>
-      </Link>
-      <Link to='/messages'>
-        <button>Messages</button>
-      </Link>
+      <Nav />
     </>
   );
 };
