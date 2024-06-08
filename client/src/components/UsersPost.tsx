a62df3921829975e68fa6ab285edc340974cd2f7
cody-daigle <cody.h.daigle@gmail.com>
2024-06-08 12:54:20 -0500

    Added User's post component

---
+++ client/src/components/UsersPost.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+interface Post {
+  title: string;
+  body: string;
+}
+interface PostProps {
+  post: Post;
+}
+
+const UsersPost = ({ post }: PostProps): React.ReactElement => {
+  console.log('post', post);
+  return (
+    <>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+    </>
+  );
+};
+
+export default UsersPost;
