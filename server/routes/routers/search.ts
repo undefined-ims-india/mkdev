8ea0d4e8b8e606a87a5a3b0a070a424d0cd3ac59
Hackman78 <pathenry231@gmail.com>
2024-06-08 18:45:14 -0500

    added get request based on tagType and tags

--- server/routes/routers/search.ts
+++ server/routes/routers/search.ts
@@ -1,9 +1,49 @@
 import { Router, Response, Request } from 'express';
-
+import { PrismaClient, tagType } from '@prisma/client';
 const search = Router();
+const prisma = new PrismaClient();
 
-search.get('/', (req: Request, res: Response) => {
-  res.status(200).send('you rang!');
+
+
+search.get('/:tagType/:tags', async (req: Request, res: Response) => {
+    const { tagType: tagTypeParam, tags } = req.params;
+    const splitTags = tags.split('-');
+    console.log(splitTags, tagTypeParam, tags);
+
+    // Validate and cast tagTypeParam to tagType enum
+    if (!Object.values(tagType).includes(tagTypeParam as tagType)) {
+        return res.status(400).send('Invalid tag type');
+    }
+
+    try {
+        const searchResults = await prisma.tags.findMany({
+            where: {
+                tagType: tagTypeParam as tagType, // Cast to tagType enum
+                name: { in: splitTags }
+            },
+            include: {
+                posts: true,
+                user: true
+            }
+        });
+
+        res.status(200).json(searchResults);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+search.post('/', (req: Request, res: Response) => {
+    res.status(200).send('you rang!');
+});
+
+search.put('/', (req: Request, res: Response) => {
+    res.status(200).send('you rang!');
+});
+
+search.delete('/', (req: Request, res: Response) => {
+    res.status(200).send('you rang!');
 });
 
 export default search;
