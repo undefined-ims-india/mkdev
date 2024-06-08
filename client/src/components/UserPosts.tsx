4f99ad4141b1b753d7e0452c4959f34793d55cbc
cody-daigle <cody.h.daigle@gmail.com>
2024-06-08 17:51:32 -0500

    Commented out to correct bugs

--- client/src/components/UserPosts.tsx
+++ client/src/components/UserPosts.tsx
@@ -1,23 +1,38 @@
-import React, { ReactElement } from 'react';
-import UsersPost from './UsersPost';
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-}
-interface ProfileProps {
-  posts: Post[];
-}
+// import React, { useState, ReactElement, useEffect } from 'react';
+// import axios from 'axios';
+// import UsersPost from './UsersPost';
+// // interface Post {
+// //   id: number;
+// //   title: string;
+// //   body: string;
+// // }
+// // interface ProfileProps {
+// //   posts: Post[];
+// // }
 
-const UserPosts = ({ posts }: ProfileProps): ReactElement => {
-  console.log('posts', posts);
-  return (
-    <>
-      {posts.map((post) => (
-        <UsersPost key={post.id} post={post} />
-      ))}
-    </>
-  );
-};
+// const UserPosts = ({ userId }: { userId: number }): ReactElement => {
+//   const [posts, setPosts] = useState([]);
 
-export default UserPosts;
+//   useEffect(() => {
+//     const getPosts = async () => {
+//       try {
+//         const response = await axios.get(`/posts/user/${userId}`);
+//         setPosts(response.data);
+//       } catch (error) {
+//         console.error('Failed to fetch user posts:', error);
+//         // Here you can handle the error, for example, set an error state
+//       }
+//     };
+
+//     getPosts();
+//   }, [userId]);
+//   return (
+//     <>
+//       {posts.map((post) => (
+//         <UsersPost key={post.id} post={post} />
+//       ))}
+//     </>
+//   );
+// };
+
+// export default UserPosts;
