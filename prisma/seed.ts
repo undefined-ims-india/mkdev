9f74b771aee36bf6b4a75d9891bbd5e48e2e6ff6
cody-daigle <cody.h.daigle@gmail.com>
2024-06-10 15:26:12 -0500

    Corrected syntax errors in seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,20 +3,20 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-    // Clear the old seed data
-    await prisma.post.deleteMany().then(() => console.log('Deleted all posts'));
-    await prisma.user.deleteMany().then(() => console.log('Deleted all users'));
+  // Clear the old seed data
+  await prisma.post.deleteMany().then(() => console.log('Deleted all posts'));
+  await prisma.user.deleteMany().then(() => console.log('Deleted all users'));
 
-    const tagJavaScript = await prisma.tags.create({
-        data: { name: "javascript", tagType: "post" }
-    });
-    const tagTypescript = await prisma.tags.create({
-        data: { name: "typescript", tagType: "post" }
-    });
+  const tagJavaScript = await prisma.tags.create({
+    data: { name: 'javascript', tagType: 'post' },
+  });
+  const tagTypescript = await prisma.tags.create({
+    data: { name: 'typescript', tagType: 'post' },
+  });
 
   const user1 = await prisma.user.create({
     data: {
-      username: 'cody.h.daigle'
+      username: 'cody.h.daigle',
       googleId: '1234567890',
       firstName: 'Cody',
       lastName: 'Daigle',
@@ -28,12 +28,15 @@ async function main() {
       posts: {
         create: [
           { title: 'Interesting day!', body: 'This is the first post.' },
-          { title: 'Typescript', body: 'A love/hate language.',
-          tags: { 
+          {
+            title: 'Typescript',
+            body: 'A love/hate language.',
+            tags: {
               connect: {
-                  id: tagTypescript.id
-               }
-           },
+                id: tagTypescript.id,
+              },
+            },
+          },
         ],
       },
     },
@@ -51,18 +54,21 @@ async function main() {
       post_count: 2,
       posts: {
         create: [
-          { title: 'Messages', body: "I'm implementing a messages feature!",
-          tags: {
-                            connect: { id: tagJavaScript.id },
-                        },
+          {
+            title: 'Messages',
+            body: "I'm implementing a messages feature!",
+            tags: {
+              connect: { id: tagJavaScript.id },
+            },
           },
           {
             title: 'Conversations',
             body: 'It can store all conversations from everyone!',
-            tags: { 
+            tags: {
               connect: {
-                  id: tagTypescript.id
-               }
+                id: tagTypescript.id,
+              },
+            },
           },
         ],
       },
@@ -79,27 +85,30 @@ async function main() {
       follower_count: 10,
       post_count: 2,
 
-            posts: {
-                create: [
-                    {
-                        title: 'Searching!',
-                        body: "User's can search tags or other users!",
-              tags: { 
-                  connect: {
-                      id: tagTypescript.id
-               },
-                    },
-                    { title: 'Dashboard', body: 'Not sure who is doing that yet...'
-                      tags: { 
-                            connect: {
-                                       id: tagTypescript.id
-                              }
-                       },
-                ],
+      posts: {
+        create: [
+          {
+            title: 'Searching!',
+            body: "User's can search tags or other users!",
+            tags: {
+              connect: {
+                id: tagTypescript.id,
+              },
             },
-        },
-    });
-
+          },
+          {
+            title: 'Dashboard',
+            body: 'Not sure who is doing that yet...',
+            tags: {
+              connect: {
+                id: tagTypescript.id,
+              },
+            },
+          },
+        ],
+      },
+    },
+  });
 
   const user4 = await prisma.user.create({
     data: {
@@ -112,36 +121,41 @@ async function main() {
       follower_count: 10,
       post_count: 2,
 
-            posts: {
-                create: [
-                    {
-                        title: 'EZPZ',
-                        body: "Working on some S3 buckets for user's Demos!",
-                        tags: { 
-                            connect: {
-                                id: tagTypescript.id
-                         }
-                    },
-                    { title: 'Scrum', body: 'Boooo!',
-                      tags: { 
-                        connect: {
-                            id: tagTypescript.id
-                    },
-                  },
-                ],
+      posts: {
+        create: [
+          {
+            title: 'EZPZ',
+            body: "Working on some S3 buckets for user's Demos!",
+            tags: {
+              connect: {
+                id: tagTypescript.id,
+              },
             },
-        },
-    });
+          },
 
-    console.log({ user1, user2, user3, user4 });
-    console.log('Database seeded with 4 users and 8 posts');
+          {
+            title: 'Scrum',
+            body: 'Boooo!',
+            tags: {
+              connect: {
+                id: tagTypescript.id,
+              },
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  console.log({ user1, user2, user3, user4 });
+  console.log('Database seeded with 4 users and 8 posts');
 }
 
 main()
-    .catch((e) => {
-        console.error(e);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
