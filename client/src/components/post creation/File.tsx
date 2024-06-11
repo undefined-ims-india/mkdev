75f31191d6f0a1d1e11a1e7bc3b3e8c7797daa59
Alex <aphebert2000@gmail.com>
2024-06-11 16:29:14 -0500

    ADD component files

---
+++ client/src/components/post creation/File.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const File = ():React.ReactElement => {
+  return (<></>)
+}
+
+export default File;
---
+++ client/src/components/post creation/Repo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+
+const Repo = ():React.ReactElement => {
+  return (<></>)
+}
+
+export default Repo;
