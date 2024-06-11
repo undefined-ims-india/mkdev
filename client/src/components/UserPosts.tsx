abc023f74760ae73511cebd56134a72058d69b9c
cody-daigle <cody.h.daigle@gmail.com>
2024-06-11 09:04:19 -0500

    Removed unused comments and updated User interface

--- client/src/components/UserPosts.tsx
+++ client/src/components/UserPosts.tsx
@@ -1,13 +1,11 @@
-
 import React, { useState, ReactElement, useEffect } from 'react';
 import axios from 'axios';
 import UsersPost from './UsersPost';
-import { PortalProps } from '@mui/material';
+
 interface User {
   id: number;
   firstName: string;
   lastName: string;
-  email: string;
   linkedIn: string;
   github: string;
   sub: string;
@@ -36,26 +34,4 @@ const UserPosts = ({ posts }: ProfileProps): ReactElement => {
   );
 };
 
-//   useEffect(() => {
-//     const getPosts = async () => {
-//       try {
-//         const response = await axios.get(`/posts/user/${userId}`);
-//         setPosts(response.data);
-//       } catch (error) {
-//         console.error('Failed to fetch user posts:', error);
-//         // Here you can handle the error, for example, set an error state
-//       }
-//     };
-
-//     getPosts();
-//   }, [userId]);
-//   return (
-//     <>
-//       {posts.map((post) => (
-//         <UsersPost key={post.id} post={post} />
-//       ))}
-//     </>
-//   );
-// };
-
- export default UserPosts;
+export default UserPosts;
