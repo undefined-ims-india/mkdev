f14e908ebbe668e092b89d8d07982e3706813914
cody-daigle <cody.h.daigle@gmail.com>
2024-06-11 20:31:12 -0500

    Loading all user's posts within MUI Tab

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, ReactElement } from 'react';
+import React, { useState, useEffect, ReactElement, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Nav from './Nav';
@@ -8,6 +8,9 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
+import TabPanel from '@mui/lab/TabPanel';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
 
 interface User {
   id: number;
@@ -43,10 +46,12 @@ const Profile = (): ReactElement => {
   const [followersCount, setFollowersCount] = useState<number>(0);
   const [blogs, setBlogs] = useState<Blog[]>([]);
   const [username, setUsername] = useState<string>('');
-  const [value, setValue] = React.useState(0);
+  const userRef = useRef(user);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
+  const [tab, setTab] = useState('1');
+
+  const handleTab = (event: React.SyntheticEvent, newTab: string) => {
+    setTab(newTab);
   };
 
   const getUser = () => {
@@ -69,33 +74,32 @@ const Profile = (): ReactElement => {
       ? user.name
       : user.username;
   };
+  useEffect(() => {
+    const userId = user?.id;
+    if (userId) {
+      axios
+        .get<Post[]>(`/api/posts/user/${userId}`)
+        .then(({ data }) => {
+          setPosts(data);
+          // console.log('posts', data);
+        })
+        .catch((error) => {
+          console.error('Failed to fetch posts:', error);
+        });
+    }
+  }, [user]);
 
   useEffect(() => {
     setUsername(checkUsername());
     getUser();
-  }, [user]);
-
-  // const getPosts = () => {
-  //   axios
-  //     .get(`/api/posts/user/${user?.id}`)
-  //     .then(({ data }) => {
-  //       setPosts(data);
-  //       console.log('posts', data);
-  //     })
-  //     .catch((error) => {
-  //       console.error('Failed to fetch posts:', error);
-  //     });
-  // };
-  // useEffect(() => {
-  //   getPosts();
-  // }, [user]);
+  }, [userRef]);
 
   return (
     <div>
       <Nav />
       {user && (
         <div>
-          <p>{`${username}`}</p>
+          <h4>{`${username}`}</h4>
           <img
             src={user?.picture}
             alt={user?.name}
@@ -103,21 +107,30 @@ const Profile = (): ReactElement => {
           />
           <p>{user?.linkedinId} LinkedIn Link</p>
           <p>{user?.githubId} Github Link</p>
-          <div
-            style={{
-              border: '1px solid black',
-              padding: 1,
-              borderRadius: 12,
-            }}
-          >
-            <UserPosts posts={posts} />
-          </div>
           <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-            <Tabs value={value} onChange={handleChange} centered>
-              <Tab label='Item One' />
-              <Tab label='Item Two' />
-              <Tab label='Item Three' />
-            </Tabs>
+            <TabContext value={tab}>
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <TabList onChange={handleTab} aria-label='lab API tabs example'>
+                  <Tab label='Posts' value='1' />
+                  <Tab label='Item Two' value='2' />
+                  <Tab label='Item Three' value='3' />
+                </TabList>
+              </Box>
+              <TabPanel value='1'>
+                {<UserPosts posts={posts} getPosts={function (): void {}} />}
+              </TabPanel>
+              <TabPanel value='2'>{<Blogs />}</TabPanel>
+              <TabPanel value='3'>Item Three</TabPanel>
+            </TabContext>
+            <div
+              style={{
+                border: '1px solid black',
+                padding: 1,
+                borderRadius: 12,
+              }}
+            >
+              {/* <UserPosts posts={posts} /> */}
+            </div>
           </Box>
         </div>
       )}
