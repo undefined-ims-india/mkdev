a72b592fb4b3ed844aad050335cb9f6cacf0f22e
Alex <aphebert2000@gmail.com>
2024-06-11 19:43:43 -0500

    ADD can load a repo, select a file and view it

--- client/src/components/post creation/Repo.tsx
+++ client/src/components/post creation/Repo.tsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, ReactElement} from 'react';
 import axios from 'axios';
+import MarkDown from '../MarkDown';
 
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
@@ -11,26 +12,32 @@ import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Repo = ():ReactElement => {
 
   const [tree, setTree]: [{path:string}[], Function] = useState([]);
   const [username, setUsername] = useState('AlexPHebert2000');
   const [repoName, setRepoName] = useState('blackjack');
-
-  useEffect(() => {
-  }, [])
+  const [displayFile, setDisplayFile] = useState('');
 
   const handleTextInput = (e:React.ChangeEvent<HTMLInputElement>):void => {
     switch(e.target.name) {
       case 'username': setUsername(e.target.value); break;
       case 'repo-name': setRepoName(e.target.value); break;
     }
-  }
+  };
 
   const handleLookup = async (e:React.MouseEvent<HTMLButtonElement>):Promise<void> => {
     const { data }: {data: {path:string, type:string}[]} = await axios.get(`/api/repos/${username}/${repoName}/tree`)
     setTree(data.filter(({type}) => type === 'blob'))
+  };
+
+  const changePath = async (e: React.MouseEvent<HTMLButtonElement> | any):Promise<void> => {
+    const { data } = await axios.get(`/api/repos/${username}/${repoName}/${e.target.name}/contents`)
+    setDisplayFile(`\`\`\`\n${data}\n\`\`\``);
   }
 
   return (
@@ -39,12 +46,11 @@ const Repo = ():ReactElement => {
     <TextField value={repoName} name="repo-name" onChange={handleTextInput} />
     <Button onClick={handleLookup}>Add Repo</Button>
     <Box>
-      <List>
-        {tree.map((file, index) => (
-          <ListItem key={index}><Button>{file.path}</Button></ListItem>
-        ))}
-      </List>
-    </Box>
+        <List>
+          {tree.map((file, index) => (<ListItem key={index}><Button onClick={changePath} name={file.path}>{file.path}</Button></ListItem>))}
+        </List>
+        <MarkDown text={displayFile} />
+      </Box>
   </>
   )
 }
