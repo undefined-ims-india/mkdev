27fbf09d8bf86366a02ba54ad39e651077f9e314
Alex <aphebert2000@gmail.com>
2024-06-11 21:52:31 -0500

    DELETE migrations folder

--- prisma/migrations/20240604165742_user_defaults_edit/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Post" ALTER COLUMN "repoLink" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "follower_count" SET DEFAULT 0,
-ALTER COLUMN "post_count" SET DEFAULT 0;
