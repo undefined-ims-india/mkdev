390f4b6c212d73646656c54d5a9983e8e7a5369b
Alex <aphebert2000@gmail.com>
2024-06-13 17:20:29 -0500

    REMOVE unused user routers

--- server/routes/routers/users.ts
+++ server/routes/routers/users.ts
@@ -5,41 +5,6 @@ import { PrismaClient } from '@prisma/client';
 const users = Router();
 const prisma = new PrismaClient();
 
-users.post('/', (req: any, res: any) => {
-  const {
-    newUser,
-  }: { newUser: { name: string; username: string; firstName: string } } =
-    req.body;
-  prisma.user
-    .create({ data: newUser })
-    .then((user: any) => {
-      console.log(user);
-      res.sendStatus(201);
-    })
-    .catch((err: { name: string }) => {
-      console.error(err);
-      res.sendStatus(500);
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
-    });
-});
-
-users.get('/', (req: any, res: any) => {
-  prisma.user
-    .findMany()
-    .then((users: {}[]) => {
-      res.send(users);
-    })
-    .catch((err: { name: string }) => {
-      console.error(err);
-      res.sendStatus(500);
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
-    });
-});
-
 // Authenticated route to verify a user is logged in
 users.get('/loggedIn', (req: any, res: any) => {
   const user = req.user;
