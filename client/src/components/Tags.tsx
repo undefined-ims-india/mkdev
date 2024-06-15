ee1f704a8649fa316768cb64634f0fe7d0147884
Hackman78 <pathenry231@gmail.com>
2024-06-15 16:20:38 -0500

    fixup! rewrite for proper react syntax

--- client/src/components/Tags.tsx
+++ client/src/components/Tags.tsx
@@ -3,45 +3,48 @@ import Chip from '@mui/material/Chip';
 import axios from 'axios';
 
 interface TagsResponse {
-  id: number;
-  tagType: 'User' | 'Post';
-  name: string;
+	id: number;
+	tagType: 'User' | 'Post';
+	name: string;
 }
 
 const Tags = () => {
-  const [userTags, setUserTags] = useState<TagsResponse[]>([]);
+	const [userTags, setUserTags] = useState<TagsResponse[]>([]);
 
-  const getUserTags = async () => {
-    try {
-      const { data } = await axios.get('/api/tags');
-      setUserTags(data.tags);
-    } catch (error) {
-      console.error('Error fetching user tags:', error);
-    }
-  };
+	const getUserTags = async () => {
+		try {
+			const { data } = await axios.get('/api/tags');
+			setUserTags(data.tags);
+		} catch (error) {
+			console.error('Error fetching user tags:', error);
+		}
+	};
 
-  useEffect(() => {
-    getUserTags();
-  }, []);
+	useEffect(() => {
+		getUserTags();
+	}, []);
 
-  const handleClick = async (id: number) => {
-    console.log(id, 'YOU CLICKED!!');
-    try {
-      await axios.patch(`/api/tags/${id}`);
-      getUserTags();
-    } catch (error) {
-      console.error('Error deleting tag:', error);
-    }
-  };
+	const handleClick = async (id: number) => {
+		console.log(id, 'YOU CLICKED!!');
+		try {
+			await axios.patch(`/api/tags/${id}`);
+			getUserTags();
+		} catch (error) {
+			console.error('Error deleting tag:', error);
+		}
+	};
 
-  return (
-    <div>
-      {/* <Chip onClick={handleClick} label='testing' /> */}
-      {userTags.map((tag) => (
-     <Chip key={tag.id} label={tag.name} onClick={() => handleClick(tag.id)} />
-      ))}
-    </div>
-  );
+	return (
+		<div>
+			{userTags.map((tag) => (
+				<Chip
+					key={tag.id}
+					label={tag.name}
+					onClick={() => handleClick(tag.id)}
+				/>
+			))}
+		</div>
+	);
 };
 
 export default Tags;
--- server/routes/routers/tags.ts
+++ server/routes/routers/tags.ts
@@ -4,68 +4,77 @@ const tags = Router();
 const prisma = new PrismaClient();
 
 // get tags for the given user
-tags.get('/',async (req: Request, res: Response) => {
-    // const { id } = req.user.id;
-    const id = 1;
-    try {
-    const tagResponse = await prisma.user.findUnique({
-        where: {
-            id: +id,
-        },
-        select: {
-            tags: true,
-            },
-        })
-        res.status(201).send(tagResponse);
-    } catch {
-        res.status(500).send('User Not Found');
-    }
+tags.get('/', async (req: Request, res: Response) => {
+	// const { id } = req.user.id;
+	const id = 1;
+	try {
+		const tagResponse = await prisma.user.findUnique({
+			where: {
+				id: +id,
+			},
+			select: {
+				tags: true,
+			},
+		});
+		res.status(201).send(tagResponse);
+	} catch {
+		res.status(500).send('User Not Found');
+	}
+});
+
+tags.get('/all', async (req: Request, res: Response) => {
+	try {
+    const tags = await prisma.tags.findMany()
+
+    res.status(200).send(tags)
+	} catch (error) {
+		res.status(500).send('Error getting all tags from user');
+	}
 });
 
 // Handler to add tags to the user
 tags.post('/:tagId', async (req: Request, res: Response) => {
-    const { tagId } = req.params;
-    // const { id } = req.user.id;
-    const id = 1; 
-  
-    try {
-      const updatedUser = await prisma.user.update({
-        where: { id: +id },
-        data: {
-          tags: {
-            connect: { id: +tagId },
-          },
-        },
-        select: { tags: true },
-      });
-      console.log('tag post', updatedUser);
-      res.status(200).send(updatedUser);
-    } catch (error) {
-      res.status(500).send('Error adding tag to user');
-    }
-  });
+	const { tagId } = req.params;
+	// const { id } = req.user.id;
+	const id = 1;
 
+	try {
+		const updatedUser = await prisma.user.update({
+			where: { id: +id },
+			data: {
+				tags: {
+					connect: { id: +tagId },
+				},
+			},
+			select: { tags: true },
+		});
+		console.log('tag post', updatedUser);
+		res.status(200).send(updatedUser);
+	} catch (error) {
+		res.status(500).send('Error adding tag to user');
+	}
+});
 
 //patch request to remove a tag from a users array of tags
 tags.patch('/:tagId', async (req: Request, res: Response) => {
-    const { tagId } = req.params;
-    // const { id } = req.user.id;
-    const id = 1; 
-  
-    try {
-      const updatedUser = await prisma.user.update({
-        where: { id: +id },
-        data: {
-          tags: {
-            disconnect: { id: +tagId },
-          },
-        },
-        select: { tags: true },
-      });
-      res.status(200).send(updatedUser);
-    } catch (error) {
-      res.status(500).send('Error updating user tags');
-    }
-  });
+	const { tagId } = req.params;
+	// const { id } = req.user.id;
+	const id = 1;
+
+	try {
+		const updatedUser = await prisma.user.update({
+			where: { id: +id },
+			data: {
+				tags: {
+					disconnect: { id: +tagId },
+				},
+			},
+			select: { tags: true },
+		});
+		res.status(200).send(updatedUser);
+	} catch (error) {
+		res.status(500).send('Error updating user tags');
+	}
+});
 
 export default tags;
