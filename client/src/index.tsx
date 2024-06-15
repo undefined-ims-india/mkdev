71fb87c1d59dfb7f609d46540c07a7d26f4afd3d
mike <sammartino.mike@gmail.com>
2024-06-15 14:49:56 -0400

    wrap BrowserRouter in ThemeProvider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,10 +4,22 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ThemeProvider>
 );
 
 // root.render(<App />)
