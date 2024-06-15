2c507af5207e0c27844c56df9b67adbb1e63c5e8
mike <sammartino.mike@gmail.com>
2024-06-15 17:21:51 -0400

    remove theme.ts file

--- client/src/styling/theme.ts
+++
@@ -1,6 +0,0 @@
-// look up themes from materialui, see what properties I may need
-
-export const lightTheme = {};
-export const darkTheme = {};
-export const mutedTheme = {};
-export const highContrastTheme = {};
