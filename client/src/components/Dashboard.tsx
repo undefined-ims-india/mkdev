95fe489fb449fcbac8444d14bbf5b9b3c9fa5a65
AlexPHebert2000 <aphebert9994@gmail.com>
2024-06-15 15:04:01 -0500

    FIX refactored dashboard feed to use Post.tsx

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -5,6 +5,8 @@ import Post from './Post';
 import axios from 'axios';
 import { PostWithRelations } from '../../../types';
 
+import Box from '@mui/material/Box';
+
 const Dashboard = (): ReactElement => {
 
   const [feed, setFeed]:[PostWithRelations[], Function] = useState([]);
@@ -14,16 +16,18 @@ const Dashboard = (): ReactElement => {
     axios.get('/api/feed')
       .then(({data}) => {
         setFeed(data);
+        console.log('hi')
       })
   }, [feedRef])
 
   return (
     <>
-      <Nav />
       <SearchComponent />
+      <Box>
         {feed.map((post) => (
-          <Post content={post} />
+          <Post key={post.id + post.title} content={post} />
         ))}
+      </Box>
     </>
   );
 };
