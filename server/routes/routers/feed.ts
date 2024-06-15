b63768a43a5ae1f2088b84aa4ae679244293c7bb
AlexPHebert2000 <aphebert9994@gmail.com>
2024-06-15 15:02:49 -0500

    ADD tags to the include of feed posts

--- server/routes/routers/feed.ts
+++ server/routes/routers/feed.ts
@@ -1,12 +1,33 @@
 import express, { Router } from 'express';
-import { PrismaClient, User } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const feed = Router();
 const prisma = new PrismaClient();
 
 feed.get('/', async (req: any, res: any):Promise<void> => {
-    const allPosts = await prisma.post.findMany({include: {author: true}});
-    res.send(allPosts.map(post => ({...post, author: post.author.username, userId: post.author.id})).reverse());
+  try{
+    const allPosts = await prisma.post.findMany(
+      {
+        include: {
+          author: true,
+        tags: true,
+        },
+        orderBy: [
+          {
+            createdAt: 'desc'
+          }
+        ]
+      }
+    );
+    res.send(allPosts);
+  }
+  catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+  finally {
+    await prisma.$disconnect();
+  }
 })
 
 export default feed;
