2ecf1042b889f7cfee585b10cc4f9945b829af89
cody-daigle <cody.h.daigle@gmail.com>
2024-06-15 11:46:10 -0500

    Updated UserProfileType to include followedBy & follwing

--- types.ts
+++ types.ts
@@ -1,12 +1,14 @@
 import { User, Post, Tags, Blog } from '@prisma/client';
 
 export interface UserProfileType extends User {
-  posts: PostWithRelations[],
-  tags: Tags[],
-  blogs: Blog[]
+  posts: PostWithRelations[];
+  tags: Tags[];
+  blogs: Blog[];
+  followedBy: User[];
+  following: User[];
 }
 
 export interface PostWithRelations extends Post {
-  author: User,
-  tags: Tags[]
+  author: User;
+  tags: Tags[];
 }
