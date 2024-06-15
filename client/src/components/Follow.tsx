f624b0fd7052a0e5cb7138ac74cde9d3b65c9ceb
cody-daigle <cody.h.daigle@gmail.com>
2024-06-15 15:59:18 -0500

    Add / Follow button component to dynamically change depending on status

--- client/src/components/Follow.tsx
+++ client/src/components/Follow.tsx
@@ -1,132 +1,50 @@
 import React, { ReactElement, useState, useEffect } from 'react';
-import Button from '@mui/material/Button';
 import { useParams } from 'react-router-dom';
+import { UserProfile } from '../../../types';
 import axios from 'axios';
-import TabPanel from '@mui/lab/TabPanel';
 
-interface User {
-  followers: any;
-  id: number;
-  name: string;
-  firstName: string;
-  lastName: string;
-  linkedinId: string;
-  githubId: string;
-  devId: string;
-  username: string;
-  picture: string;
-  followedBy: User[];
-  following: User[];
-}
-interface UserProps {
-  id: number;
-  user: User;
-  userId: number;
-  following: User[];
-  select: (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    followedId: number
-  ) => void;
-}
-
-const Follow = ({ userId, select }: UserProps): ReactElement => {
-  let { id } = useParams();
-  const [user, setUser] = useState<User>({} as User);
-  const [following, setFollowing] = useState<User[]>([]);
-  const [followedBy, setFollowedBy] = useState<User[]>([]);
+const Follow = (): ReactElement => {
+  const { id } = useParams<{ id: string }>();
+  const [userProfile, setUserProfile] = useState<UserProfile | null>(null);
   const [isFollowing, setIsFollowing] = useState(false);
 
-  const selectFollow = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    followedId: number
-  ): void => {
-    select(e, followedId);
-  };
-
-  const follow = (followedId: number) => {
-    axios
-      .post('/api/follows/follow', {
-        followerId: user?.id,
-        followedId: followedId,
-      })
-      .then(({ data }) => {
-        setIsFollowing(true);
-        setFollowing((prevList) => [...prevList, data.followedUser]);
-      })
-      .catch((err) => console.error('Failed to Follow user:', err));
-  };
-
   useEffect(() => {
-    // Get follower List
-    axios
-      .get(`/api/follows/followers/${user?.id}`)
-      .then(({ data }) => {
-        console.log(data);
-        setFollowedBy(data);
-      })
-      .catch((err) => console.error('Could not get list of Followers:', err));
+    axios.get(`/api/follows/${id}`).then(({ data }) => {
+      setUserProfile(data);
+      setIsFollowing(data.followedBy.includes(id));
+    });
 
-    // Get following List
-    axios
-      .get(`/api/follows/following/${user?.id}`)
-      .then(({ data }) => setFollowing(data.following))
-      .catch((err) => console.error('Could not get list of Following:', err));
-  }, [user?.id]);
+    axios.get(`/api/users/${id}/followers/count`).then(({ data }) => {
+      setUserProfile((prevState) =>
+        prevState ? { ...prevState, follower_count: data.count } : null
+      );
+    });
+  }, [id]);
+
+  const follow = () => {
+    axios.post(`/api/follows/follow`).then(() => {
+      setIsFollowing(true);
+    });
+  };
+
+  const unfollow = () => {
+    axios.delete(`/api/follows/unfollow`).then(() => {
+      setIsFollowing(false);
+    });
+  };
 
   return (
-    <>
-      <TabPanel value='3'>
-        {user.followers.length > 0 ? (
-          <Follow
-            following={user.followers}
-            id={user.id}
-            user={user}
-            userId={user.id}
-            select={selectFollow}
-          />
-        ) : (
-          <p>No followers</p>
-        )}
-      </TabPanel>
-
-      <TabPanel value='4'>
-        {user.following.length > 0 ? (
-          <div>
-            <h2>Followers</h2>
-            {followedBy.map((follower) => (
-              <div key={follower.id}>
-                <h3>{follower.name}</h3>
-                <p>{follower.picture}</p>
-                <Button
-                  onClick={(e) => {
-                    selectFollow(e, follower.id);
-                  }}
-                >
-                  Follow
-                </Button>
-              </div>
-            ))}
-
-            <h2>Following</h2>
-            {following.map((followedUser) => (
-              <div key={followedUser.id}>
-                <h3>{followedUser.name}</h3>
-                <p>{followedUser.picture}</p>
-                <Button
-                  onClick={(e) => {
-                    selectFollow(e, followedUser.id);
-                  }}
-                >
-                  Unfollow
-                </Button>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p>Not following anyone</p>
-        )}
-      </TabPanel>
-    </>
+    <div>
+      {!userProfile ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <button onClick={isFollowing ? unfollow : follow}>
+            {isFollowing ? 'Unfollow' : 'Follow'}
+          </button>
+        </>
+      )}
+    </div>
   );
 };
 
