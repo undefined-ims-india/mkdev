99962333edc03e00aa26f8f6e1b9007086472b65
mike <sammartino.mike@gmail.com>
2024-06-15 17:25:06 -0400

    remove theme related code from index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,20 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-
 createRoot(document.getElementById('root')!).render(
-    <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </ThemeProvider>
 );
