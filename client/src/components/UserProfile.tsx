a869319d270fe5f8f8a7cb1bc0ae731eff6af4c6
AlexPHebert2000 <aphebert9994@gmail.com>
2024-06-15 11:19:00 -0500

    ADD styling and skeleton loader

--- client/src/components/UserProfile.tsx
+++ client/src/components/UserProfile.tsx
@@ -4,14 +4,23 @@ import { useParams } from 'react-router-dom';
 import { UserProfileType } from '../../../types';
 import Nav from './Nav';
 import Post from './Post';
+import Blogs from './Blogs';
 
-import Box from '@mui/material/Box'
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import Tab from '@mui/material/Tab';
+import TabPanel from '@mui/lab/TabPanel';
+import Skeleton from '@mui/material/Skeleton';
+
 
 const UserProfile = ():React.ReactElement => {
 
-  let { id } = useParams();
-  let [profileData, setProfileData]: [UserProfileType | null, Function] = useState(null);
-  let profileDataREF = useRef(profileData)
+  const { id } = useParams();
+  const [profileData, setProfileData]: [UserProfileType | null, Function] = useState(null);
+  const profileDataREF = useRef(profileData)
+  const [tab, setTab] = useState('1');
 
   useEffect(() => {
     axios.get(`/api/users/${id}/profile`)
@@ -20,19 +29,57 @@ const UserProfile = ():React.ReactElement => {
       })
   }, [profileDataREF])
 
+  const handleTab = (e: React.SyntheticEvent<Element, Event>, value: string) => {
+    setTab(value);
+  };
+
   try {
     return (
       <>
         <Nav />
           <Box>
-            <h1>{profileData!.username}</h1>
+            <h4>{profileData!.username}</h4>
+            <Avatar
+              src={profileData!.picture !== null ? profileData!.picture : ''}
+              alt={profileData!.username !== null ? profileData!.username : profileData!.name !== null ? profileData!.name : ''}
+            >
+              {profileData!.username![0]}
+            </Avatar>
+            <p>LinkedIn</p>
+            <p><a href={`https://dev.to/${profileData!.devId}`}>Dev.to</a></p>
+            <p><a href={`https://github.com/${profileData!.githubId}`}>Github</a></p>
           </Box>
-          {profileData!.posts.map((post) => <Post key={post.title + crypto.randomUUID()} content={post} />)}
+          <Box>
+            <TabContext value={tab}>
+              <Box>
+                <TabList onChange={handleTab}>
+                  <Tab label='Posts' value='1'/>
+                  <Tab label='Dev.to BLogs' value='2' />
+                </TabList>
+              </Box>
+              <TabPanel value='1'>
+                {profileData!.posts.map((post) => <Post key={post.title + crypto.randomUUID()} content={post} />)}
+              </TabPanel>
+              <TabPanel value='2'>
+                <Blogs devId={profileData!.devId !== null ? profileData!.devId : ''} />
+              </TabPanel>
+            </TabContext>
+          </Box>
       </>
     )
   }
   catch {
-    return <>{'Please Wait'}</>
+    return (
+      <>
+        <Nav />
+        <Skeleton />
+        <Skeleton variant='circular' width={50} height={50}/>
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton variant='rectangular' height={500} />
+      </>
+    )
   }
 }
 
