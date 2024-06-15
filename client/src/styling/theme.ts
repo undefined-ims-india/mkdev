8f5296bbd9fcfd7698afdcff17bc36d73d2efb2e
mike <sammartino.mike@gmail.com>
2024-06-14 21:07:39 -0400

    add theme file to export themes from

---
+++ client/src/styling/theme.ts
@@ -0,0 +1,6 @@
+// look up themes from materialui, see what properties I may need
+
+export const lightTheme = {};
+export const darkTheme = {};
+export const mutedTheme = {};
+export const highContrastTheme = {};
