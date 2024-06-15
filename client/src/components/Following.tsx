2ca56d4d0e7eea9b57e6f4a1f51f2ff7158c19f8
cody-daigle <cody.h.daigle@gmail.com>
2024-06-15 13:32:52 -0500

    Add / Following components for the user's a user is following

---
+++ client/src/components/Following.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { UserProfile } from '../../../types';
+
+const Following = (): React.ReactElement => {
+  const { id } = useParams();
+  let [followingData, setFollowingData]: [UserProfile[] | null, Function] =
+    useState<UserProfile[] | null>(null);
+
+  useEffect(() => {
+    axios.get(`/api/follows/following/${id}`).then(({ data }) => {
+      console.log('following', data);
+      setFollowingData(data);
+    });
+  }, [id]);
+
+  return (
+    <div>
+      <h3>Following</h3>
+      <ul>
+        {followingData &&
+          followingData.map((following) => (
+            <li key={following.id}>{following.name}</li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Following;
