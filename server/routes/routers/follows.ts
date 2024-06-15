50ebbb1a7dbe81b3b61c8ed04cc0cb6165046102
cody-daigle <cody.h.daigle@gmail.com>
2024-06-15 15:38:01 -0500

    Removed unused code

--- server/routes/routers/follows.ts
+++ server/routes/routers/follows.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { PrismaClient } from '@prisma/client';
 
 const follow = Router();
@@ -10,13 +10,11 @@ follow.post('/follow', async (req: any, res: any) => {
   console.log('followingId', followingId);
 
   try {
-    // Add to user's following list
     await prisma.user.update({
       where: { id: +id },
       data: { following: { connect: { id: followingId } } },
     });
 
-    // Add user to followed user's follower list
     await prisma.user.update({
       where: { id: followingId },
       data: { followedBy: { connect: { id: +id } } },
@@ -79,13 +77,10 @@ follow.post('/unfollow', async (req: any, res: any) => {
   const { userId } = req.params;
 
   try {
-    // Remove from user's following list
     await prisma.user.update({
       where: { id: userId },
       data: { following: { disconnect: { id: followingId } } },
     });
-
-    // Remove user to followed user's follower list
     await prisma.user.update({
       where: { id: followingId },
       data: { followedBy: { disconnect: { id: userId } } },
@@ -94,33 +89,6 @@ follow.post('/unfollow', async (req: any, res: any) => {
     res.sendStatus(201);
   } catch (err) {
     res.sendStatus(500);
-  }
-});
-
-// Not sure If this will be needed at the moment
-
-// Get followers and following
-follow.get('/user/:id', async (req: any, res: any) => {
-  const { id } = req.params;
-
-  try {
-    const followersAndFollowing = await prisma.user.findUnique({
-      where: { id: id },
-      include: {
-        followedBy: true,
-        following: true,
-      },
-    });
-
-    if (followersAndFollowing) {
-      return res.json(followersAndFollowing);
-    }
-
-    res.sendStatus(404);
-  } catch (err) {
-    res.sendStatus(500);
-  } finally {
-    await prisma.$disconnect();
   }
 });
 export default follow;
