c713d00c099b6d16a4c306391c8d2016df5a95c6
AlexPHebert2000 <aphebert9994@gmail.com>
2024-06-16 19:36:13 -0500

    FIX align post component contents

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -1,24 +1,31 @@
 import React, {ReactElement, useState} from "react";
 import { PostWithRelations } from "../../../types";
 import { Link } from "react-router-dom";
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
+
 import MarkDown from "./MarkDown";
 
 import Card from "@mui/material/Card";
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 const Post = ({content} : {content: PostWithRelations}): ReactElement => {
   return (
     <Card sx={{marginBottom: 5}}>
-      <Box sx={{display: 'flex', flexDirection:'row'}}>
+      <Box sx={{display:"flex", flexDirection:'row', marginLeft: 1, marginTop: 1, alignItems: 'center'}}>
         <Link to={`/user/${content.author.id}/profile`}>
-        <Avatar alt={content.author.username!} src={content.author.picture!}>
-          {'?'}
-          </Avatar>
+          <Avatar alt={content.author.username!} src={content.author.picture!}>
+            {'?'}
+            </Avatar>
         </Link>
-        {content.author.username || content.author.name}
+        <Typography variant="h1" sx={{fontSize: 20, marginLeft: 2, marginRight: 2}}>{content.author.username || content.author.name}</Typography>
+        <Typography variant="body2" sx={{color: 'lightgrey'}}>{dayjs(content.createdAt).fromNow()}</Typography>
       </Box>
-      <Box sx={{display:"flex", flexDirection:'column'}}>
+      <Box sx={{display:"flex", flexDirection:'column', marginLeft: 2}}>
         <MarkDown text={content.title} />
         <MarkDown text={content.body} />
       </Box>
