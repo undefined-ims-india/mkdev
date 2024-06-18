ab2eb082c8476e481e31b205868d1b6eeb2ec2da
cody-daigle <cody.h.daigle@gmail.com>
2024-06-18 17:38:38 -0500

    Added logout function to remove user from being logged in through google

--- client/src/components/Logout.tsx
+++ client/src/components/Logout.tsx
@@ -1,10 +1,23 @@
 import React, { ReactElement } from 'react';
+import axios from 'axios';
 
 const Logout = (): ReactElement => {
+  React.useEffect(() => {
+    axios
+      .post('/logout')
+      .then(({ data }) => {
+        console.log(data);
+        if (!data.ok) {
+          console.log('Logout failed');
+        }
+        window.location.href = '/login';
+      })
+      .catch((err) => console.error('Logout failed:', err));
+  }, []);
+
   return (
     <>
       <h2>You have successfully logged out!</h2>
-      <h3>Refreshing automatically routes back to App page...</h3>
     </>
   );
 };
