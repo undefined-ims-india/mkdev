04dfd94fe828bffc08583a8adce7d62372bfe6c8
AlexPHebert2000 <aphebert9994@gmail.com>
2024-06-18 17:03:39 -0500

    ADD avatar to nav bar

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,15 +1,28 @@
-import React, { ReactElement, useContext } from 'react';
+import React, { ReactElement, useContext, useState, useEffect } from 'react';
 import { UserContext } from './UserContext';
 import { Link } from 'react-router-dom';
 import { ThemeToggle } from '../styling/ThemeToggle';
+import axios from 'axios';
 
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
 
 const Nav = (): ReactElement => {
 
   const id = useContext(UserContext);
+  const [profileImage, setProfileImage]: [string, Function] = useState('null');
+  useEffect(() => {
+    axios.get(`/api/users/${id}/image`)
+      .then(({data}):void => {
+        setProfileImage(data.picture);
+        console.info(data)
+      })
+      .catch((err: Error) => {
+        console.error(err);
+      });
+  }, [profileImage, id])
 
   return (
     <Box sx={{display: 'flex', flexDirection:'row', alignItems:'center', width: '100%', justifyContent: 'space-between'}}>
@@ -18,7 +31,7 @@ const Nav = (): ReactElement => {
           <Typography variant='h1' sx={{fontSize: 36}}>mkDev</Typography>
         </Link>
       </Box>
-      <Box>
+      <Box sx={{display: 'flex', flexDirection:'row'}}>
         {!!id ?
         <>
           <Link to={`/user/${id}/profile`}>
@@ -33,6 +46,7 @@ const Nav = (): ReactElement => {
           <Link to='/logout'>
             <Button>Logout</Button>
           </Link>
+          <Avatar src={profileImage} >?</Avatar>
         </>
         :
         <Link to='/login'>
