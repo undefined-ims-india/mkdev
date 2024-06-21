160253ad1b8affda6d4fe96edd4a0aabe67bf8c5
Hackman78 <pathenry231@gmail.com>
2024-06-21 17:22:24 -0500

    fixed formatting

--- client/src/components/Drawers.tsx
+++ client/src/components/Drawers.tsx
@@ -6,55 +6,65 @@ import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 
 interface Props {
-  mobileOpen: boolean;
-  onTransitionEnd: () => void;
-  handleDrawerClose: () => void;
-  handleDrawerTransitionEnd: () => void;
-  handleDrawerToggle: () => void;
+	mobileOpen: boolean;
+	onTransitionEnd: () => void;
+	handleDrawerClose: () => void;
+	handleDrawerTransitionEnd: () => void;
+	handleDrawerToggle: () => void;
 }
 
-const drawerWidth = 240; 
+const drawerWidth = 240;
 
 export default function Drawers(props: Props) {
-  const {
-    handleDrawerClose,
-    mobileOpen,
-    onTransitionEnd,
-    handleDrawerTransitionEnd,
-  } = props;
+	const {
+		handleDrawerClose,
+		mobileOpen,
+		onTransitionEnd,
+		handleDrawerTransitionEnd,
+	} = props;
 
-  return (
-    <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }} aria-label="mailbox folders">
-      <Drawer
-        variant="temporary"
-        open={mobileOpen}
-        onTransitionEnd={handleDrawerTransitionEnd}
-        onClose={handleDrawerClose}
-        ModalProps={{
-          keepMounted: true, 
-        }}
-        sx={{
-          display: { xs: 'block', sm: 'none' },
-          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-        }}
-      >
-        <Box>
-          <IconButton onClick={handleDrawerClose}>
-            <CloseIcon />
-          </IconButton>
-          <Tags />
-        </Box>
-      </Drawer>
-      <Drawer
-        variant="permanent"
-        sx={{
-          display: { xs: 'none', sm: 'block' },
-          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-        }}
-        open
-      >
-        <Tags />
-      </Drawer>
-    </Box>
-  );
+	return (
+		<Box
+			component='nav'
+			sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+			aria-label='mailbox folders'
+		>
+			<Drawer
+				variant='temporary'
+				open={mobileOpen}
+				onTransitionEnd={handleDrawerTransitionEnd}
+				onClose={handleDrawerClose}
+				ModalProps={{
+					keepMounted: true,
+				}}
+				sx={{
+					display: { xs: 'block', sm: 'none' },
+					'& .MuiDrawer-paper': {
+						boxSizing: 'border-box',
+						width: drawerWidth,
+					},
+				}}
+			>
+				<Box>
+					<IconButton onClick={handleDrawerClose}>
+						<CloseIcon />
+					</IconButton>
+					<Tags />
+				</Box>
+			</Drawer>
+			<Drawer
+				variant='permanent'
+				sx={{
+					display: { xs: 'none', sm: 'block' },
+					'& .MuiDrawer-paper': {
+						boxSizing: 'border-box',
+						width: drawerWidth,
+					},
+				}}
+				open
+			>
+				<Tags />
+			</Drawer>
+		</Box>
+	);
 }
