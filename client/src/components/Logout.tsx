63c3b298c39255ce2cfb1bbbf84a32c1731b1d72
cody-daigle <cody.h.daigle@gmail.com>
2024-06-21 16:18:31 -0500

    Fix / terminates user session

--- client/src/components/Logout.tsx
+++ client/src/components/Logout.tsx
@@ -1,24 +1,28 @@
 import React, { ReactElement, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { redirect, useNavigate } from 'react-router-dom';
 
 import axios from 'axios';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Login from './Login';
 
 const Logout = (): ReactElement => {
   const navigate = useNavigate();
-  
+
   useEffect(() => {
-    axios.post('/logout').then(({ data }): void => {
-      if (!data.ok) {
-        console.log('Logout failed');
-      }
-        navigate('/login');
-    });
-  }, []);
+    axios
+      .post('/logout')
+      .then(() => navigate('/login'))
+      .catch((err) => console.error(err));
+  }, [navigate]);
 
   return (
-    <>
-      <h2>You have successfully logged out!</h2>
-    </>
+    <Box>
+      <Typography variant='h2' align='center' gutterBottom>
+        You have successfully logged out!
+      </Typography>
+      <Login />
+    </Box>
   );
 };
 
