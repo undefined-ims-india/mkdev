04a28860213aff83e65f0ef824e4deb6030dec72
AlexPHebert2000 <aphebert9994@gmail.com>
2024-06-21 19:16:40 -0500

    FIX post creation page styling

--- client/src/components/PostCreationPage.tsx
+++ client/src/components/PostCreationPage.tsx
@@ -9,49 +9,49 @@ import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import Input from '@mui/material/Input';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import Tab from '@mui/material/Tab';
+import TabPanel from '@mui/lab/TabPanel';
 
 const PostCreationPage = (): ReactElement => {
   const navigate = useNavigate();
-  const [title, setTitle]: [string, Function] = useState('');
+  const [title, setTitle]: [string, Function] = useState('# ');
   const [body, setBody]: [string, Function] = useState('');
   const [titleFieldTooltip, setTitleFieldTooltip] = useState(false);
   const [bodyFieldTooltip, setBodyFieldTooltip] = useState(false);
-  const [img, setImg]: [any, Function] = useState();
+  // const [img, setImg]: [any, Function] = useState();
   const [cantSubmit, setCantSubmit]: [boolean, Function] = useState(false);
   const [repo, setRepo]: [{link: string, files: { path: string; contents: string }[]},Function] = useState({link:'', files:[]});
+  const [currentTab, setCurrentTab] = useState('0');
 
+  const handleTabChange = (e: React.SyntheticEvent, newValue: string) => {
+    setCurrentTab(newValue);
+  }
 
   const handleTextInput = (
     e:
       | React.ChangeEvent<HTMLInputElement>
       | React.ChangeEvent<HTMLTextAreaElement>,
   ): void => {
-    let setStateFunc: Function;
-    if (e.target.name === 'title') {
-      if (titleFieldTooltip) {
-        setTitleFieldTooltip(false);
-      }
-      setStateFunc = setTitle;
-    } else if (e.target.name === 'body') {
-      if (bodyFieldTooltip) {
-        setBodyFieldTooltip(false);
-      }
-      setStateFunc = setBody;
-    } else {
-      return;
+    switch (e.target.name) {
+      case 'title': {setTitle(`# ${e.target.value}`); break;}
+      case 'body': {setBody(e.target.value); break;}
     }
-    setStateFunc(e.target.value);
   };
 
-  const handleFile = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    console.log(e.target.files);
-    setImg(e.target.files![0]);
-  };
+  // const handleFile = (e: React.ChangeEvent<HTMLInputElement>): void => {
+  //   console.log(e.target.files);
+  //   setImg(e.target.files![0]);
+  // };
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
     setCantSubmit(true);
-    axios.postForm('/api/posts', { title, body, img, repo:btoa(JSON.stringify(repo)) })
+    axios.postForm('/api/posts', { title, body, /*img,*/ repo:btoa(JSON.stringify(repo)) })
       .then(({ data }) => {
         navigate('/dashboard');
     })
@@ -69,47 +69,66 @@ const PostCreationPage = (): ReactElement => {
   };
 
   return (
-    <div>
-      <h1>Create Post</h1>
-      <Box sx={{display:'flex', flexDirection: 'row'}}>
-        <Stack>
-          <Input
-            id="image-upload"
-            type="file"
-            name="img"
-            onChange={handleFile}
-          />
-          <Input
-            id="post-title"
-            type="text"
-            value={title}
-            onChange={handleTextInput}
-            name="title"
-            placeholder="Title"
-          />
-          <Input
-            id="post-body"
-            type="text"
-            multiline
-            value={body}
-            onChange={handleTextInput}
-            name="body"
-            placeholder="Body Text"
-            rows={4}
-          />
-          <Button onClick={handleSubmit} disabled={cantSubmit}>
-            Submit
-          </Button>
-        </Stack>
-        <Divider orientation='vertical' />
-        <Stack>
-          <MarkDown text={title} />
-          <MarkDown text={body} />
-        </Stack>
-      </Box>
-      <Divider />
-      <Repo saveFile={saveFile} saveRepo={saveRepo}/>
-    </div>
+    <Grid container spacing={0}>
+      <Grid item xs />
+      <Grid item xs={8}>
+        <Paper elevation={3}>
+          <Box sx={{ display:'flex', flexDirection: 'row', alignItems:'center'}}>
+            <Typography variant='h1'>Create Post</Typography>
+          </Box>
+          <Divider orientation='horizontal' variant='middle'/>
+
+          <TabContext value={currentTab}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <TabList onChange={handleTabChange}>
+                <Tab label="Edit" value="0"/>
+                <Tab label="Preview" value="1"/>
+                <Tab label="Repo" value="2"/>
+              </TabList>
+            </Box>
+            <TabPanel value="0">
+              <Paper>
+                <Stack sx={{marginRight: 2, marginLeft: 2}}>
+                  <Input
+                    id="post-title"
+                    type="text"
+                    value={title.slice(2)}
+                    onChange={handleTextInput}
+                    name="title"
+                    placeholder="Title"
+                    />
+                  <Input
+                    id="post-body"
+                    type="text"
+                    multiline
+                    value={body}
+                    onChange={handleTextInput}
+                    name="body"
+                    placeholder="Body Text"
+                    rows={30}
+                    />
+                </Stack>
+              </Paper>
+            </TabPanel>
+            <TabPanel value="1">
+              <Paper>
+                <Stack>
+                  <MarkDown text={title} />
+                  <Divider orientation='horizontal' variant='middle' />
+                  <MarkDown text={body} />
+                </Stack>
+              </Paper>
+            </TabPanel>
+            <TabPanel value="2">
+              <Paper>
+                <Repo saveFile={saveFile} saveRepo={saveRepo}/>
+              </Paper>
+            </TabPanel>
+          </TabContext>
+        </Paper>
+      </Grid>
+      <Grid item xs />
+    </Grid>
   );
 };
 
