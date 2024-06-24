a301c5e4262f3479ef05e7849a851d7fbe97f747
AlexPHebert2000 <aphebert9994@gmail.com>
2024-06-24 00:03:14 -0500

    FIX black background for code blocks

--- client/src/components/MarkDown.tsx
+++ client/src/components/MarkDown.tsx
@@ -3,11 +3,13 @@ import parse from 'html-react-parser';
 import markdownit from 'markdown-it';
 const md = markdownit();
 
+import Box from '@mui/material/Box';
+
 const MarkDown = ({text}:{text: string}) :ReactElement => {
   return (
-    <>
+    <Box className='markdown-container'>
       {parse(md.render(text))}
-    </>
+    </Box>
   )
 }
 
