c3218720f922cf1b3e12c880ef582fd9d5ae52be
cody-daigle <cody.h.daigle@gmail.com>
2024-07-01 16:11:21 -0500

    Corrected the <p> issue with Typography

--- client/src/components/profile/Socials.tsx
+++ client/src/components/profile/Socials.tsx
@@ -14,67 +14,73 @@ interface UserProps {
 
 const Socials = ({ profileData }: UserProps): ReactElement => {
   return (
-    <Grid item xs={12} sm={6} md={4} lg={3}>
-      <Box
-        display='flex'
-        flexDirection='row'
-        alignItems='center'
-        justifyContent={'space-evenly'}
-      >
-        <Typography variant='body1'>
-          {profileData!.linkedinId ? (
-            <Link
-              href={`https://www.linkedin.com/in/${profileData!.linkedinId}`}
-              target='_blank'
-              title='LinkedIn Profile'
-            >
-              <LinkedInIcon fontSize='large' />
-            </Link>
-          ) : (
-            <LinkedInIcon fontSize='large' color='disabled' />
-          )}
-        </Typography>
-        <Typography variant='body1'>
-          {profileData!.githubId ? (
-            <Link
-              href={`https://github.com/${profileData!.githubId}`}
-              target='_blank'
-              title='GitHub Profile'
-            >
-              <GitHubIcon fontSize='large' />
-            </Link>
-          ) : (
-            <GitHubIcon fontSize='large' color='disabled' />
-          )}
-        </Typography>
-        <Typography variant='body1'>
-          {profileData!.devId ? (
-            <Link
-              href={`https://dev.to/${profileData!.devId}`}
-              target='_blank'
-              title='Dev.to Profile'
-            >
-              Dev.to
-            </Link>
-          ) : (
-            <Typography color='textSecondary'>"Dev.to"</Typography>
-          )}
-        </Typography>
-        <Typography variant='body1'>
-          {profileData!.mediumId ? (
-            <Link
-              href={`https://medium.com/@${profileData!.mediumId}`}
-              target='_blank'
-              title='Medium.dev Profile'
-            >
-              Medium
-            </Link>
-          ) : (
-            <Typography color='textSecondary'>"Medium"</Typography>
-          )}
-        </Typography>
-      </Box>
-    </Grid>
+    <>
+      <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Box
+          display='flex'
+          flexDirection='row'
+          alignItems='center'
+          justifyContent={'space-evenly'}
+        >
+          <Typography variant='body1' component='div'>
+            {profileData!.linkedinId ? (
+              <Link
+                href={`https://www.linkedin.com/in/${profileData!.linkedinId}`}
+                target='_blank'
+                title='LinkedIn Profile'
+              >
+                <LinkedInIcon fontSize='large' />
+              </Link>
+            ) : (
+              <LinkedInIcon fontSize='large' color='disabled' />
+            )}
+          </Typography>
+          <Typography variant='body1' component='div'>
+            {profileData!.githubId ? (
+              <Link
+                href={`https://github.com/${profileData!.githubId}`}
+                target='_blank'
+                title='GitHub Profile'
+              >
+                <GitHubIcon fontSize='large' />
+              </Link>
+            ) : (
+              <GitHubIcon fontSize='large' color='disabled' />
+            )}
+          </Typography>
+          <Typography variant='body1' component='div'>
+            {profileData!.devId ? (
+              <Link
+                href={`https://dev.to/${profileData!.devId}`}
+                target='_blank'
+                title='Dev.to Profile'
+              >
+                Dev.to
+              </Link>
+            ) : (
+              <Typography color='disabled' component='span'>
+                Dev.to
+              </Typography>
+            )}
+          </Typography>
+          <Typography variant='body1' component='div'>
+            {profileData!.mediumId ? (
+              <Link
+                href={`https://medium.com/@${profileData!.mediumId}`}
+                target='_blank'
+                title='Medium.dev Profile'
+              >
+                Medium
+              </Link>
+            ) : (
+              <Typography color='disabled' component='span'>
+                Medium
+              </Typography>
+            )}
+          </Typography>
+        </Box>
+      </Grid>
+    </>
   );
 };
 
