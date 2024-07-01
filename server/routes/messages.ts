e5c418d1c428f7b4699f4c488311a334faaac7b7
mike <sammartino.mike@gmail.com>
2024-07-01 17:52:39 -0400

    add nested read query for message POST response

--- server/routes/messages.ts
+++ server/routes/messages.ts
@@ -37,25 +37,36 @@ messages.post('/:conversationId', (req: Request, res: Response) => {
       conversation: {
         connect: { id: conversationId }
       }
+    },
+    include: {
+      sender: {
+        select: {
+          username: true,
+          picture: true,
+        }
+      }
     }
   })
-  .then((message) => {
-    // query for username and avatar
-    prisma.user.findUnique({
-      where: { id: message.senderId }
-    })
-    .then((user) => {
-      res.status(201).send({
-        ...message,
-        username: user!.username,
-        picture: user!.picture
-      })
-    })
-    .catch((err) => {
-      console.error('Failed to find user associated with that id:\n', err);
-      res.sendStatus(404);
-    })
+  .then((data) => {
+    res.status(201).send(data);
   })
+  // .then((message) => {
+  //   // query for username and avatar
+  //   prisma.user.findUnique({
+  //     where: { id: message.senderId }
+  //   })
+  //   .then((user) => {
+  //     res.status(201).send({
+  //       ...message,
+  //       username: user!.username,
+  //       picture: user!.picture
+  //     })
+  //   })
+  //   .catch((err) => {
+  //     console.error('Failed to find user associated with that id:\n', err);
+  //     res.sendStatus(404);
+  //   })
+  // })
   .catch((err: Error) => {
     console.error('Failed to create new message:\n', err);
     res.sendStatus(500);
