8d3c380833857cbc6b4a4f0d8c0db34a038769d0
cody-daigle <cody.h.daigle@gmail.com>
2024-07-01 18:38:43 -0500

    Added profileInfo card to hold all user info in component

--- client/src/components/profile/ProfileInfo.tsx
+++ client/src/components/profile/ProfileInfo.tsx
@@ -1,9 +1,10 @@
 import React, { ReactElement, useContext } from 'react';
-import { Avatar, Box, IconButton, Typography } from '@mui/material';
+import { Avatar, Box, Card, Grid, IconButton } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import Follow from './Follow';
 import { UserProfile } from '../../../../types';
 import { UserContext } from '../UserContext';
+import Socials from './Socials';
 
 interface ProfileProps {
   profileData: UserProfile;
@@ -16,28 +17,39 @@ const ProfileInfo = ({
 }: ProfileProps): ReactElement => {
   const userId = useContext(UserContext);
   return (
-    <>
-      <Avatar
-        sx={{
-          width: 120,
-          height: 120,
-          mt: 2,
-          borderColor: 'primary.main',
-          border: '2px solid',
-        }}
-        src={profileData.picture !== null ? profileData.picture : ''}
-        alt={profileData.username || profileData.name || ''}
-      />
-      <Box display='flex' justifyContent='center' mt={2} mb={2}>
-        {userId === profileData.id ? (
-          <IconButton aria-label='edit' onClick={handleEdit}>
-            <EditIcon />
-          </IconButton>
-        ) : (
-          <Follow />
-        )}
+    <Card sx={{ maxWidth: 300, margin: 'auto', mt: 2 }}>
+      <Box display='flex' justifyContent='center' mb={2}>
+        <Grid
+          container
+          direction='row'
+          alignItems='center'
+          justifyContent='center'
+          spacing={3}
+        >
+          <Grid item xs={12} sm={6} md={8} lg={9}>
+            <Box display='flex' flexDirection='column' alignItems='center'>
+              <Avatar
+                sx={{ width: 100, height: 100, mt: 2 }}
+                src={profileData!.picture !== null ? profileData!.picture : ''}
+                alt={profileData!.username || profileData!.name || ''}
+              />
+              <Box display='flex' justifyContent='center' mt={2} mb={2}>
+                {userId === profileData!.id ? (
+                  <IconButton aria-label='edit' onClick={handleEdit}>
+                    <EditIcon />
+                  </IconButton>
+                ) : (
+                  <Follow />
+                )}
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
-    </>
+      <Box>
+        <Socials profileData={profileData!} />
+      </Box>
+    </Card>
   );
 };
 
