5cbc91cfb4ea24af05edb0b1fdf8bbdcf5a650ad
cody-daigle <cody.h.daigle@gmail.com>
2024-07-02 10:24:39 -0500

    Added loading and refactored request to be more efficient when accepting both medium & dev.to

--- client/src/components/profile/Blogs.tsx
+++ client/src/components/profile/Blogs.tsx
@@ -7,87 +7,69 @@ import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import Link from '@mui/material/Link';
-import { User } from '@prisma/client';
+import { BlogPosts } from '../../../../types';
 import Box from '@mui/material/Box';
 
-interface Blog {
-  id: number;
-  title: string;
-  url: string;
-  cover_image: string;
-  description: string;
-}
-interface UserProps {
-  devId: string;
-  mediumId: string;
-}
-
-const Blogs = ({ devId, mediumId }: UserProps): ReactElement => {
-  const [blogs, setBlogs] = useState<Blog[]>([]);
+const Blogs = ({ devId, mediumId }): ReactElement => {
+  const [blogs, setBlogs] = useState<BlogPosts[]>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     setBlogs([]);
-    // Dev.to Blogs
-    const getDev = () => {
-      axios
-        .get(`https://dev.to/api/articles?username=${devId}&per_page=6`)
-        .then(({ data }) => {
-          setBlogs((prevBlogs) => [...prevBlogs, ...data]);
-        })
-        .catch((err) => console.error(err));
-    };
-    // Medium Blogs
-    const getMedium = () => {
-      axios
-        .get(
-          `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${mediumId}&per_page=6`
-        )
-        .then(({ data }) => {
-          const medBlogs = data.items.map((blog: any) => ({
-            id: blog.guid,
-            title: blog.title,
-            description: blog.description,
-            url: blog.link,
-            cover_image: blog.thumbnail,
-          }));
-          setBlogs((prevBlogs) => [...prevBlogs, ...medBlogs]);
-        })
-        .catch((err) => console.error(err));
+
+    const getDevBlogs = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://dev.to/api/articles?username=${devId}&per_page=6`
+        );
+        setBlogs((prevBlogs) => [...prevBlogs, ...data]);
+      } catch (error) {
+        console.error(error);
+      }
     };
 
-    if (devId) {
-      getDev();
-    }
-    if (mediumId) {
-      getMedium();
-    }
+    const getMediumBlogs = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${mediumId}&per_page=6`
+        );
+        const medBlogs = data.items.map((blog: any) => ({
+          id: blog.guid,
+          title: blog.title,
+          description: blog.description,
+          url: blog.link,
+          cover_image: blog.thumbnail,
+        }));
+        setBlogs((prevBlogs) => [...prevBlogs, ...medBlogs]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    Promise.all([getDevBlogs(), getMediumBlogs()]).finally(() =>
+      setLoading(false)
+    );
   }, [devId, mediumId]);
 
   return (
     <Box>
       <Grid container spacing={4}>
-        {blogs.length === 0 ? (
+        {loading ? (
           <Grid item xs={12}>
-            <Typography
-              variant='h1'
-              component='h2'
-              fontSize={'1rem'}
-              align='center'
-            >
-              No Blogs Found
-            </Typography>
+            <Typography align='center'>Loading...</Typography>
           </Grid>
-        ) : (
+        ) : blogs.length ? (
           blogs.map((blog, idx) => (
             <Grid item key={idx} xs={12} sm={6} md={4}>
               <Card>
-                {/* <AspectRatio> */}
-                {/* <Link href={blog.url}>{`${blog.cover_image}`}</Link> */}
-                <CardMedia
-                  component='img'
-                  image={blog.cover_image}
-                  alt={blog.title}
-                />
+                <Link href={blog.url} target='_blank'>
+                  <CardMedia
+                    component='img'
+                    image={blog.cover_image}
+                    alt={blog.title}
+                  />
+                </Link>
                 <CardContent>
                   <Typography
                     variant='h1'
@@ -101,6 +83,17 @@ const Blogs = ({ devId, mediumId }: UserProps): ReactElement => {
               </Card>
             </Grid>
           ))
+        ) : (
+          <Grid item xs={12}>
+            <Typography
+              variant='h1'
+              component='h2'
+              fontSize={'1rem'}
+              align='center'
+            >
+              No Blogs Found
+            </Typography>
+          </Grid>
         )}
       </Grid>
     </Box>
