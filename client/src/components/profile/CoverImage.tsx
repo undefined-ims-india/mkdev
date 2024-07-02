ff1d14b9ce15f666f82405d83a207e6be0b42dcd
cody-daigle <cody.h.daigle@gmail.com>
2024-07-02 12:02:34 -0500

    Added favicon image

---
+++ client/src/components/profile/CoverImage.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+const CoverImage = (): ReactElement => {
+  return (
+    <div>
+      <img src='https://imgs.search.brave.com/R8WE85ZpdRmyw4M2DaVpySj1aJBxOP4HQrghtTyebzE/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLmt5/bS1jZG4uY29tL3Bo/b3Rvcy9pbWFnZXMv/bmV3c2ZlZWQvMDAw/LzM3Ny85NDYvMGI5/LmpwZw' />
+    </div>
+  );
+};
+
+export default CoverImage;
