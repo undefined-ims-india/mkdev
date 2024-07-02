6d9a4e27608961e540fefc9c72e48e55c0670abb
AlexPHebert2000 <aphebert9994@gmail.com>
2024-07-02 13:08:05 -0500

    ADD see full post button to card

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -33,7 +33,7 @@ const Post = ({content, refreshParent} : {content: PostWithRelations, refreshPar
     <Card className="glass-card" sx={{
       borderRadius: 2,
       maxWidth: '55vw',
-      backgroundColor: 'rgba(37, 55, 60, 0.67)',
+      backgroundColor: 'rgba(27, 45, 50, 0.47)',
       backdropFilter: 'blur(14px) saturate(180%)',
       border: '1px solid rgba(255, 255, 255, 0.125)',
       boxShadow: '0 1px 12px rgba(0, 0, 0, 0.25)',
@@ -43,31 +43,42 @@ const Post = ({content, refreshParent} : {content: PostWithRelations, refreshPar
         {content!.s3_key ? <img alt="cover image" src={`https://mkdev-ims-india.s3.us-east-2.amazonaws.com/${content!.s3_key}`} /> : <></>}
       </Box>
       <Grid container spacing={0} sx={{background:'aliceblue', padding: '1vh',}} className={content.s3_key ? "bottom-curve" : "rounded"}>
-        <Grid item lg={3} xs={12}>
-          <Box sx={{display: "flex", flexDirection: "row", overflow: 'wrap', justifyContent: "start", alignItems: 'center'}}>
-            <Link to={`/user/${content.author.id}/profile`} style={{textDecoration: 'none'}}>
-              <Avatar
-                alt={content.author.username! || content.author.name}
-                src={content.author.picture!}
-                sx={{
-                  width: '2.5vw', height: '2.5vw',
-                  minWidth: 40, minHeight: 40
-                }}
-              >
-                {content.author.username![0] || content.author.firstName[0] || '?'}
-              </Avatar>
-            </Link>
-            <Box sx={{display:"flex", flexDirection:'column',  marginLeft: 2, marginRight: 2, flexGrow: 1}}>
-              <Typography variant="h1" sx={{fontSize: 23}}>{content.author.username || content.author.name}</Typography>
-              <Typography variant="body2" sx={{color: 'silver'}}>{dayjs(content.createdAt).fromNow()}</Typography>
-            </Box>
+        <Grid item lg={3} xs={12} sx={{display: "flex", flexDirection: "row", overflow: 'wrap', justifyContent: "start", alignItems: 'center'}}>
+          <Link to={`/user/${content.author.id}/profile`} style={{textDecoration: 'none'}}>
+            <Avatar
+              alt={content.author.username! || content.author.name}
+              src={content.author.picture!}
+              sx={{
+                width: '2.5vw', height: '2.5vw',
+                minWidth: 40, minHeight: 40
+              }}
+            >
+              {content.author.username![0] || content.author.firstName[0] || '?'}
+            </Avatar>
+          </Link>
+          <Box sx={{display:"flex", flexDirection:'column',  marginLeft: 2, marginRight: 2, flexGrow: 1}}>
+            <Typography variant="h1" sx={{fontSize: 23}}>{content.author.username || content.author.name}</Typography>
+            <Typography variant="body2" sx={{color: 'silver'}}>{dayjs(content.createdAt).fromNow()}</Typography>
           </Box>
         </Grid>
         <Grid item lg={9} xs={0}/>
-        <Grid item xs={12}>
-          <Box sx={{overflow: 'wrap'}}>
-            <MarkDown text={content.title} />
-          </Box>
+        <Grid item xs={12} sx={{overflow: 'wrap'}}>
+          <MarkDown text={content.title} />
+        </Grid>
+        <Grid item xs={8} sx={{display: "flex", alignContent: 'center'}}>
+          {
+          content.tags.length ?
+          <PostTagsChips tags={content.tags} /> :
+          <></>
+          }
+        </Grid>
+        <Grid item lg={2} xs={0}/>
+        <Grid item lg={2} xs={4} sx={{display: 'flex', justifyContent: 'right'}}>
+          <Link to={`/post/${content.id}`}>
+            <Button>
+              {'See Full Post'}
+            </Button>
+          </Link>
         </Grid>
       </Grid>
     </Card>
