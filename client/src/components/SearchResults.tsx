bde7abfb7ae470eebbbff49702903e89d7baf8c9
Hackman78 <pathenry231@gmail.com>
2024-07-03 12:40:12 -0500

    refactored search results for users

--- client/src/components/SearchResults.tsx
+++ client/src/components/SearchResults.tsx
@@ -5,53 +5,74 @@ import Box from '@mui/material/Box';
 import SearchComponent from './Search';
 import Post from './Post';
 import { PostWithRelations } from '../../../types';
+import ProfileInfo from './profile/ProfileInfo';
+import Grid from '@mui/material/Grid';
 
 export default function SearchResults() {
-	const { tagType, tags } = useParams();
-	const [postFeed, setPostFeed]: [PostWithRelations[], Function] = useState(
-		[],
-	);
-	const [userFeed, setUserFeed] = useState([]);
-	const feedRef = useRef(postFeed);
+  const { tagType, tags } = useParams();
+  const [postFeed, setPostFeed]: [PostWithRelations[], Function] = useState([]);
+  const [userFeed, setUserFeed] = useState([]);
+  const feedRef = useRef(postFeed);
 
-	const getSearch = () => {
-		axios.get(`/api/search/filter/${tagType}/${tags}`).then(({ data }) => {
-			if (tagType === 'User') {
-				let users = data.reduce((acc: any, curr: any) => {
-					acc.push(curr.user);
-					return acc;
-				}, []);
-				console.log(users.flat());
-				return setUserFeed(users.flat());
-			}
+  const getSearch = () => {
+    axios.get(`/api/search/filter/${tagType}/${tags}`).then(({ data }) => {
+      if (tagType === 'User') {
+        let users = data.reduce((acc: any, curr: any) => {
+          acc.push(curr.user);
+          console.log(acc);
+          return acc;
+        }, []);
+        console.log(users.flat());
+        return setUserFeed(users.flat());
+      }
       // else if tagType is equal to posts
-			data.reduce((acc: any, curr: any) => {
-				acc.push(curr.posts);
-				return acc;
-			}, []);
-			return setPostFeed(data[0].posts);
-		});
-	}
+      data.reduce((acc: any, curr: any) => {
+        acc.push(curr.posts);
+        return acc;
+      }, []);
+      return setPostFeed(data[0].posts);
+    });
+  };
 
-	useEffect(() => {
-		getSearch();
-	}, []);
+  useEffect(() => {
+    getSearch();
+  }, []);
 
-	try {
-		return (
-			<Box sx={{ display: 'flex' }}>
-				<Box component='main' sx={{ flexGrow: 1, p: 3 }}>
-					<SearchComponent />
-					{postFeed.map((post) => {
+  try {
+    return (
+      <Box sx={{ display: 'flex' }}>
+        <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+          <SearchComponent />
+          {postFeed &&
+            postFeed.map((post) => {
+              return (
+                <Post
+                  key={post.id + post.title}
+                  content={post}
+                  refreshParent={getSearch}
+                />
+              );
+            })}
+          {userFeed && (
+            <Grid container spacing={2}>
+              {userFeed.map((user) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={user.id}>
+                  <ProfileInfo profileData={user} handleEdit={() => {}} />
+                </Grid>
+              ))}
+            </Grid>
+          )}
+          {/* {userFeed && userFeed.map((user) => {
+						console.log(user, 'This is user');
 						return (
-							<Post key={post.id + post.title} content={post} refreshParent={getSearch}/>
+							<ProfileInfo profileData={user} handleEdit={() => {}}/>
 						);
-					})}
-				</Box>
-			</Box>
-		);
-	} catch (err) {
-		console.error;
-		return <></>;
-	}
+					})} */}
+        </Box>
+      </Box>
+    );
+  } catch (err) {
+    console.error;
+    return <></>;
+  }
 }
