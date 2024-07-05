05300aace9fb23ef365196c1f7704892dbcc1e41
cody-daigle <cody.h.daigle@gmail.com>
2024-07-05 13:43:38 -0500

    Updating disabled/enabled social links

---
+++ client/src/components/profile/icons/DevIconDisabled.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const DevIconDisabled = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    x='0px'
+    y='0px'
+    width='50'
+    height='50'
+    viewBox='0,0,256,256'
+  >
+    <g
+      fill-opacity='0.2'
+      fill='#000000'
+      fill-rule='nonzero'
+      stroke='none'
+      stroke-width='1'
+      stroke-linecap='butt'
+      stroke-linejoin='miter'
+      stroke-miterlimit='10'
+      stroke-dasharray=''
+      stroke-dashoffset='0'
+      font-family='none'
+      font-weight='none'
+      font-size='none'
+      text-anchor='none'
+    >
+      <g transform='scale(8,8)'>
+        <path d='M2,7v18h28v-18zM4,9h24v14h-24zM6,11v10h3c1.654,0 3,-1.346 3,-3v-4c0,-1.654 -1.346,-3 -3,-3zM16,11c-1.103,0 -2,0.897 -2,2v6c0,1.103 0.897,2 2,2h2v-2h-2v-2h2v-2h-2v-2h2v-2zM19.69141,11l2.08398,9.02539c0.132,0.57 0.63961,0.97461 1.22461,0.97461c0.585,0 1.09261,-0.40461 1.22461,-0.97461l2.08398,-9.02539h-2.05273l-1.25586,5.43945l-1.25586,-5.43945zM8,13h1c0.552,0 1,0.448 1,1v4c0,0.552 -0.448,1 -1,1h-1z'></path>
+      </g>
+    </g>
+  </svg>
+);
+
+export default DevIconDisabled;
