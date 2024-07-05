c9330309dece1f7efad2b48e518342f5b3057ba7
cody-daigle <cody.h.daigle@gmail.com>
2024-07-05 15:51:41 -0500

    Updated disabled icons

--- client/src/components/profile/icons/MedIconDisabled.tsx
+++ client/src/components/profile/icons/MedIconDisabled.tsx
@@ -10,20 +10,16 @@ const MediumIconDisabled = (props: React.SVGProps<SVGSVGElement>) => (
     viewBox='0 0 300 250'
   >
     <g
-      fill-opacity='0.52157'
+      fillOpacity='0.52157'
       fill='#0b0b0b'
-      fill-rule='nonzero'
+      fillRule='nonzero'
       stroke='none'
-      stroke-width='1'
-      stroke-linecap='butt'
-      stroke-linejoin='miter'
-      stroke-miterlimit='10'
-      stroke-dasharray=''
-      stroke-dashoffset='0'
-      font-family='none'
-      font-weight='none'
-      font-size='none'
-      text-anchor='none'
+      strokeWidth='1'
+      strokeLinecap='butt'
+      strokeLinejoin='miter'
+      strokeMiterlimit='10'
+      strokeDasharray=''
+      strokeDashoffset='0'
     >
       <g transform='scale(8.53333,8.53333)'>
         <path d='M24,4h-18c-1.105,0 -2,0.895 -2,2v18c0,1.105 0.895,2 2,2h18c1.105,0 2,-0.895 2,-2v-18c0,-1.105 -0.895,-2 -2,-2zM22.542,21h-5.931v-0.333l1.389,-1.222v-6.975l-3.411,8.53h-0.533l-3.806,-8.597v6.087l1.74,2.177v0.333h-4.532v-0.333l1.799,-2.177v-7.242l-1.599,-1.999c0,0 0,-0.289 0,-0.244h4.376l3.399,7.353l2.932,-7.353h4.154v0.244l-1.519,1.277v8.919l1.542,1.222z'></path>
