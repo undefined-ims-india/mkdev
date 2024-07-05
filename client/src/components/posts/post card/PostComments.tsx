59024498e1c56ecfc6ce4827a6194ee3dac9779d
AlexPHebert2000 <aphebert9994@gmail.com>
2024-07-04 20:07:13 -0500

    ADD comment input

---
+++ client/src/components/posts/post card/PostComments.tsx
@@ -0,0 +1,69 @@
+import React, {useState, useContext} from 'react';
+import { Comment } from '../../../../../types';
+import axios from 'axios';
+import { UserContext } from '../../UserContext';
+
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import Avatar from '@mui/material/Avatar';
+import TextField from '@mui/material/TextField';
+import SendIcon from '@mui/icons-material/Send';
+import Button from '@mui/material/Button';
+
+export default ({comments}:{comments: Comment[]}) => {
+
+  const {userId, userImage} = useContext(UserContext);
+  const [body, setBody] = useState('');
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+    switch (e.target.name) {
+      case ('commentBody'): setBody(e.target.value);
+    }
+  }
+
+  const postComment = () => {
+    axios.put('/api/posts/:id/comment', {body})
+  }
+
+  return (
+    <Grid spacing={0} container className="glass-card bottom-curve" sx={{paddingY: '1vh', paddingX: "1vw"}}>
+      <Grid item xs={12} >
+        <Card sx={{display: 'flex', flexDirection: 'row', justifyContent:'center', alignItems: 'center', flexGrow: 1, paddingX: 2, paddingY: 1}}>
+          <Grid container spacing={1} >
+            <Grid item xs={1} sx={{display: 'flex', justifyContent:'center', alignItems: 'center'}}>
+              <Avatar
+              src={userImage}
+              sx={{
+                width: '2.3vw', height: '2.3vw',
+                minWidth: 30, minHeight: 30
+              }}
+              />
+            </Grid>
+            <Grid item xs={11} md={10} sx={{display: 'flex', justifyContent:'center', alignItems: 'center', paddingRight: '1vw'}}>
+              <TextField
+                onChange={handleInput}
+                name="commentBody"
+                value={body}
+                variant="outlined"
+                size="small"
+                label="Comment"
+                placeholder='Add A Comment'
+                fullWidth
+              />
+            </Grid>
+            <Grid item md={1} xs={12} sx={{display: 'flex', justifyContent:'end', alignItems: 'center'}}>
+              <Button
+                onClick={postComment}
+                variant="contained"
+                size="small"
+                >
+                <SendIcon />
+              </Button>
+            </Grid>
+          </Grid>
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
