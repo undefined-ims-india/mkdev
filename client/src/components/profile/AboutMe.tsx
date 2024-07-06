4b88357c49149e673fafc382cc32c4e33280b56d
cody-daigle <cody.h.daigle@gmail.com>
2024-07-05 19:06:28 -0500

    Updated sizing

--- client/src/components/profile/AboutMe.tsx
+++ client/src/components/profile/AboutMe.tsx
@@ -1,12 +1,11 @@
 import React, { ReactElement, useEffect, useState } from 'react';
 import { UserProfile } from '../../../../types';
 
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
+import Paper from '@mui/material/Paper';
 
 interface UserInfoProps {
   profileData: UserProfile;
@@ -18,7 +17,6 @@ const AboutMe = ({ profileData }: UserInfoProps): ReactElement => {
   if (!profileData) {
     return (
       <Grid item xs={12}>
-        <Typography align='center'>Loading...</Typography>
         <Box className='load-box'>
           <LinearProgress className='loading-bar' />
         </Box>
@@ -27,19 +25,17 @@ const AboutMe = ({ profileData }: UserInfoProps): ReactElement => {
   }
 
   return (
-    <Card sx={{ maxWidth: 600, m: 2 }}>
-      <CardContent>
-        {profileData.aboutMe.length > 0 ? (
-          <Typography variant='body1' paragraph>
-            {profileData.aboutMe}
-          </Typography>
-        ) : (
-          <Typography variant='body1' paragraph>
-            'Nothing, yet...'
-          </Typography>
-        )}
-      </CardContent>
-    </Card>
+    <Paper elevation={3} sx={{ width: 400, height: 200, m: 2, p: 2 }}>
+      {profileData.aboutMe.length > 0 ? (
+        <Typography variant='body1' paragraph>
+          {profileData.aboutMe}
+        </Typography>
+      ) : (
+        <Typography variant='body1' paragraph>
+          'Nothing, yet...'
+        </Typography>
+      )}
+    </Paper>
   );
 };
 
