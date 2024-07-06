0d23bed4fb99682ad37362f2e206d3804c592c32
cody-daigle <cody.h.daigle@gmail.com>
2024-07-05 19:43:38 -0500

    Update / UserInfo edit screen centered

--- client/src/components/profile/UserInfo.tsx
+++ client/src/components/profile/UserInfo.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { UserProfile } from '../../../../types';
+import { useNavigate } from 'react-router-dom';
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -9,6 +10,8 @@ import Input from '@mui/material/Input';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 
 interface UserInfoProps {
   profileData: UserProfile;
@@ -19,6 +22,7 @@ const UserInfo = ({
   profileData,
   UpdateUserInfo,
 }: UserInfoProps): React.ReactElement => {
+  const navigate = useNavigate();
   const [userInfo, setUserInfo]: [UserProfile | null, Function] =
     useState(profileData);
 
@@ -32,100 +36,115 @@ const UserInfo = ({
     UpdateUserInfo(userInfo!);
   };
 
+  const handleCancel = () => {
+    navigate(-1);
+  };
+
   return (
-    <Box
-      component='form'
-      sx={{ p: 1, m: 1, width: '16' }}
-      onSubmit={handleUpdate}
-    >
-      <Stack spacing={2}>
-        <FormControl sx={{ p: 2, my: 1, mx: 1, width: '10%' }}>
-          <InputLabel htmlFor='username'>Username</InputLabel>
-          <Input
-            sx={{ variant: 'contained', backgroundColor: 'white' }}
-            id='username'
-            name='username'
-            value={userInfo!.username || ''}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl sx={{ p: 2, my: 1, mx: 1, width: '10%' }}>
-          <InputLabel htmlFor='githubId'>Github Username</InputLabel>
-          <Input
-            sx={{ variant: 'contained', backgroundColor: 'white' }}
-            id='githubId'
-            name='githubId'
-            value={userInfo!.githubId || ''}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl sx={{ p: 2, my: 1, mx: 1, width: '10%' }}>
-          <InputLabel htmlFor='devId'>Dev.to Username</InputLabel>
-          <Input
-            sx={{ variant: 'contained', backgroundColor: 'white' }}
-            id='devId'
-            name='devId'
-            value={userInfo!.devId || ''}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl sx={{ p: 2, my: 1, mx: 1, width: '10%' }}>
-          <InputLabel htmlFor='mediumId'>Medium Username</InputLabel>
-          <Input
-            sx={{ variant: 'contained', backgroundColor: 'white' }}
-            id='mediumId'
-            name='mediumId'
-            value={userInfo!.mediumId || ''}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl sx={{ p: 2, my: 1, mx: 1, width: '10%' }}>
-          <InputLabel htmlFor='linkedinId'>LinkedIn Username</InputLabel>
-          <Input
-            sx={{ variant: 'contained', backgroundColor: 'white' }}
-            id='linkedinId'
-            name='linkedinId'
-            value={userInfo!.linkedinId || ''}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <Paper elevation={3} sx={{ p: 2, m: 2, maxWidth: 600 }}>
-          <Typography variant='h6' sx={{ mb: 2 }}>
-            Bio
-          </Typography>
-          <FormControl sx={{ width: '100%' }}>
-            <Input
-              id='bio'
-              name='bio'
-              multiline
-              minRows={5}
-              placeholder='This is the about me'
-              value={userInfo!.bio || ''}
-              onChange={handleChange}
-              fullWidth
-            />
-          </FormControl>
-        </Paper>
-        <Paper elevation={3} sx={{ p: 2, m: 2, maxWidth: 600 }}>
-          <Typography variant='h6' sx={{ mb: 2 }}>
-            About Me
-          </Typography>
-          <FormControl sx={{ width: '100%' }}>
-            <Input
-              id='aboutMe'
-              name='aboutMe'
-              multiline
-              minRows={5}
-              placeholder='This is the about me'
-              value={userInfo!.aboutMe || ''}
-              onChange={handleChange}
-              fullWidth
-            />
-          </FormControl>
-        </Paper>
-        <Button type='submit'>Update</Button>
-      </Stack>
-    </Box>
+    <Container>
+      <Box component='form' sx={{ p: 1, m: 1 }} onSubmit={handleUpdate}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Stack spacing={1}>
+              <FormControl sx={{ p: 2, my: 1, mx: 1, width: '50%' }}>
+                <InputLabel htmlFor='username'>Username</InputLabel>
+                <Input
+                  sx={{ variant: 'contained', backgroundColor: 'white' }}
+                  id='username'
+                  name='username'
+                  value={userInfo!.username || ''}
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <FormControl sx={{ p: 2, my: 1, mx: 1, width: '50%' }}>
+                <InputLabel htmlFor='githubId'>Github Username</InputLabel>
+                <Input
+                  sx={{ variant: 'contained', backgroundColor: 'white' }}
+                  id='githubId'
+                  name='githubId'
+                  value={userInfo!.githubId || ''}
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <FormControl sx={{ p: 2, my: 1, mx: 1, width: '50%' }}>
+                <InputLabel htmlFor='devId'>Dev.to Username</InputLabel>
+                <Input
+                  sx={{ variant: 'contained', backgroundColor: 'white' }}
+                  id='devId'
+                  name='devId'
+                  value={userInfo!.devId || ''}
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <FormControl sx={{ p: 2, my: 1, mx: 1, width: '50%' }}>
+                <InputLabel htmlFor='mediumId'>Medium Username</InputLabel>
+                <Input
+                  sx={{ variant: 'contained', backgroundColor: 'white' }}
+                  id='mediumId'
+                  name='mediumId'
+                  value={userInfo!.mediumId || ''}
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <FormControl sx={{ p: 2, my: 1, mx: 1, width: '50%' }}>
+                <InputLabel htmlFor='linkedinId'>LinkedIn Username</InputLabel>
+                <Input
+                  sx={{ variant: 'contained', backgroundColor: 'white' }}
+                  id='linkedinId'
+                  name='linkedinId'
+                  value={userInfo!.linkedinId || ''}
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <Box display='flex' justifyContent='center' gap={2}>
+                <Button type='submit' color='primary'>
+                  Update
+                </Button>
+                <Button type='button' onClick={handleCancel} color='error'>
+                  Cancel
+                </Button>
+              </Box>
+            </Stack>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper elevation={3} sx={{ p: 2, m: 2, maxWidth: 600 }}>
+              <Typography variant='h6' sx={{ mb: 2 }}>
+                Bio
+              </Typography>
+              <FormControl sx={{ width: '100%' }}>
+                <Input
+                  id='bio'
+                  name='bio'
+                  multiline
+                  minRows={5}
+                  placeholder='This is the about me'
+                  value={userInfo!.bio || ''}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </FormControl>
+            </Paper>
+            <Paper elevation={3} sx={{ p: 2, m: 2, maxWidth: 600 }}>
+              <Typography variant='h6' sx={{ mb: 2 }}>
+                About Me
+              </Typography>
+              <FormControl sx={{ width: '100%' }}>
+                <Input
+                  id='aboutMe'
+                  name='aboutMe'
+                  multiline
+                  minRows={5}
+                  placeholder='This is the about me'
+                  value={userInfo!.aboutMe || ''}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </FormControl>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 };
 
