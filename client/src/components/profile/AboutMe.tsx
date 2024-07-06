f6a1862375f36321ab044f3cbca9ad53514fd0cf
cody-daigle <cody.h.daigle@gmail.com>
2024-07-05 20:10:30 -0500

    Added 'Tags' to profile view

--- client/src/components/profile/AboutMe.tsx
+++ client/src/components/profile/AboutMe.tsx
@@ -6,6 +6,7 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
 import Paper from '@mui/material/Paper';
+import Chip from '@mui/material/Chip';
 
 interface UserInfoProps {
   profileData: UserProfile;
@@ -25,17 +26,40 @@ const AboutMe = ({ profileData }: UserInfoProps): ReactElement => {
   }
 
   return (
-    <Paper elevation={3} sx={{ width: 400, height: 200, m: 2, p: 2 }}>
-      {profileData.aboutMe.length > 0 ? (
-        <Typography variant='body1' paragraph>
-          {profileData.aboutMe}
+    <Box>
+      <Paper elevation={3} sx={{ width: 400, height: 200, m: 2, p: 2 }}>
+        {profileData.aboutMe.length > 0 ? (
+          <Typography variant='body1' paragraph>
+            {profileData.aboutMe}
+          </Typography>
+        ) : (
+          <Typography variant='body1' paragraph>
+            'Nothing, yet...'
+          </Typography>
+        )}
+      </Paper>
+      <Paper elevation={3} sx={{ width: 400, height: 200, m: 2, p: 2 }}>
+        <Typography variant='h6' fontFamily='Sometype' align='center'>
+          subscribed tags
         </Typography>
-      ) : (
-        <Typography variant='body1' paragraph>
-          'Nothing, yet...'
-        </Typography>
-      )}
-    </Paper>
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: 0.5,
+            overflow: 'hidden',
+          }}
+        >
+          {profileData.tags && profileData.tags.length > 0 ? (
+            profileData.tags.map((tag, index) => (
+              <Chip key={index} component='div' label={tag.name} />
+            ))
+          ) : (
+            <Typography variant='body2'>No interests, yet...</Typography>
+          )}
+        </Box>
+      </Paper>
+    </Box>
   );
 };
 
