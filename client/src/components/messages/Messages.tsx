77bf0575db8ee8a6d445f53161c7bf2ffd54b541
mike <sammartino.mike@gmail.com>
2024-07-08 21:19:08 -0500

    set label for top of ConversationView

--- client/src/components/messages/Messages.tsx
+++ client/src/components/messages/Messages.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, ReactElement } from 'react';
+import React, { useState, useEffect, useContext, ReactElement } from 'react';
 import ConversationList from './ConversationList';
 import ConversationView from './ConversationView';
+import { UserContext } from '../UserContext';
 
 import io from 'socket.io-client';
 import axios from 'axios';
@@ -18,6 +19,7 @@ const socket = io('http://localhost:4000');
 
 const Messages = (): ReactElement => {
 
+  const { userId } = useContext(UserContext);
   const [con, setCon] = useState<Conversations | null>();
   const [addingConversation, setAddingConversation] = useState<boolean>(false);
   const [participants, setParticipants] = useState<User[]>([]);
@@ -32,8 +34,8 @@ const Messages = (): ReactElement => {
   const getAllConversations = (): void => {
     axios
       .get('/api/conversations')
-      .then((conversations) => {
-        setAllConversations(conversations.data);
+      .then(({ data }) => {
+        setAllConversations(data);
       })
       .catch((err) => {
         setLoginError(true);
@@ -73,22 +75,32 @@ const Messages = (): ReactElement => {
     setParticipantsEntry(value);
     // iterate through participants entry and find user objects from all users
     const participantsArr: User[] = [];
-
-    // store in array to then pass in request body
     value.forEach((username) => {
       for (let i = 0; i < allUsers.length; i++) {
-        if ( allUsers[i].username === username) {
+        if (allUsers[i].username === username) {
           participantsArr.push(allUsers[i]);
         }
       }
     })
     setParticipants(participantsArr);
+  }
 
-    // set participants label
-    const label = participantsArr.reduce((acc, curr) => {
-        return acc.concat(`${curr.username}, `)
-    }, '');
-    setParticipantsLabel(label.slice(0, label.length - 2));
+  const generateConversationLabel = (con: Conversations): void => {
+    axios
+      .get(`/api/conversations/label/${con.id}`)
+      .then(({ data }) => {
+        console.log('data from getConvLabel', data);
+        let label = '';
+        for (let i = 0; i < data.participants.length; i++) {
+          if (data.participants[i].id !== userId) {
+            label += `${data.participants[i].username}, `
+          }
+        }
+        setParticipantsLabel(label.slice(0, label.length - 2))
+      })
+      .catch((err) => {
+        console.error('Failed to generate conversation label', err);
+      })
   }
 
   const addConversation = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
@@ -104,16 +116,20 @@ const Messages = (): ReactElement => {
     axios
       .post('/api/conversations', {
         participants: participants, // users that sender enters
-        label: participantsLabel
       })
-      .then((conversation) => {
-        setCon(conversation.data);
-
+      .then(({ data }) => {
+        console.log('conversation returned object', data);
+        generateConversationLabel(data);
+        return data;
+      })
+      .then((data) => {
+        setCon(data);
+        return data;
+      })
+      .then((data) => {
         socket.emit('add-conversation', {
-          id: conversation.data.id
+          id: data.id
         });
-      })
-      .then(() => {
         setAddingConversation(false);
         getAllConversations();
         setParticipantsEntry([]);
@@ -129,7 +145,8 @@ const Messages = (): ReactElement => {
     }
     setCon(newCon);
     if (newCon) {
-      setParticipantsLabel(newCon!.label);
+      // set label at top of conversation
+      generateConversationLabel(newCon);
     }
     setVisibleConversation(newCon);
   }
@@ -158,7 +175,7 @@ const Messages = (): ReactElement => {
         <>
           <Grid container>
             <Grid item>
-              <Typography variant="h1">
+              <Typography variant="h3">
                 You must be logged in to view conversations
               </Typography>
             </Grid>
@@ -232,7 +249,7 @@ const Messages = (): ReactElement => {
                         />
                       )}
                     />
-                    <Button onClick={ addConversation }>Add Conversation</Button>
+                    <Button onClick={ addConversation } variant='contained'>Create</Button>
                   </form>
                 ) : ('')
               }
