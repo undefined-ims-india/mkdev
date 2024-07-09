82ac66a06d02edbd5a3896f06860eabbad692422
cody-daigle <cody.h.daigle@gmail.com>
2024-07-09 17:24:41 -0500

    Add / Cover Image component for uploading/changing user's cover photo

--- client/src/components/profile/CoverImage.tsx
+++ client/src/components/profile/CoverImage.tsx
@@ -1,10 +1,62 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserProfile } from '../../../../types';
 
-const CoverImage = (): ReactElement => {
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import CardMedia from '@mui/material/CardMedia';
+import axios from 'axios';
+
+interface UserInfoProps {
+  profileData: UserProfile;
+}
+
+const CoverImage = ({ profileData }: UserInfoProps): ReactElement => {
+  const navigate = useNavigate();
+  const [file, setFile] = useState<File | null>(null);
+  const [userInfo, setUserInfo]: [UserProfile | null, Function] =
+    useState(profileData);
+  const [coverImage, setCoverImage] = useState('');
+
+  const handleUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    if (e.target.files && e.target.files[0]) {
+      setFile(e.target.files![0]);
+      const imageUrl = URL.createObjectURL(e.target.files![0]);
+      UpdateCoverImage({ ...profileData, coverImage: imageUrl });
+    }
+  };
+
+  const handleCancel = () => {
+    navigate(0);
+  };
+
+  const UpdateCoverImage = (userInfo: UserProfile) => {
+    axios
+      .patch(`/api/users/${userInfo.id}`, userInfo)
+      .then(({ data }): void => {
+        setUserInfo(data);
+      })
+      .catch((err) => console.error(err));
+    setUserInfo(userInfo);
+  };
+
   return (
-    <div>
-      <img src='https://imgs.search.brave.com/R8WE85ZpdRmyw4M2DaVpySj1aJBxOP4HQrghtTyebzE/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLmt5/bS1jZG4uY29tL3Bo/b3Rvcy9pbWFnZXMv/bmV3c2ZlZWQvMDAw/LzM3Ny85NDYvMGI5/LmpwZw' />
-    </div>
+    <Box component='form' onSubmit={handleUpdate}>
+      {coverImage && (
+        <CardMedia component='img' image={profileData.coverImage} alt='' />
+      )}
+      <InputLabel htmlFor='coverImage'>Upload Cover Image</InputLabel>
+      <Input id='coverImage' type='file' onChange={handleFileChange} />
+      <Button type='submit'>Update</Button>
+      <Button onClick={handleCancel}>Cancel</Button>
+    </Box>
   );
 };
 
