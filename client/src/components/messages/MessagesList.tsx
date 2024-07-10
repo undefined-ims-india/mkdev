93c9e91447958709729a3f777e2a56216bf0ab98
mike <sammartino.mike@gmail.com>
2024-07-09 20:02:01 -0500

    scroll to most recent message in conversation automatically

--- client/src/components/messages/MessagesList.tsx
+++ client/src/components/messages/MessagesList.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { useEffect, useRef, ReactElement } from 'react';
 import Message from './Message';
 
 import { Conversations } from '@prisma/client';
@@ -16,6 +16,15 @@ interface PropsType {
 
 const MessagesList: React.FC<PropsType> = ({ allMsgs, getAllMsgs, con }): ReactElement => {
 
+  const latestMsgRef = useRef<HTMLLIElement>(null);
+
+  // scroll to most recent message automatically
+  useEffect(() => {
+    if (latestMsgRef.current) {
+      latestMsgRef.current.scrollIntoView();
+    }
+  }, [allMsgs])
+
   return (
     <Box
       sx={{
@@ -28,15 +37,18 @@ const MessagesList: React.FC<PropsType> = ({ allMsgs, getAllMsgs, con }): ReactE
         {
           allMsgs.map((msg, i) => {
             if (msg.conversationId === con.id) {
-              return (
+              return i !== allMsgs.length - 1 ? (
                 <ListItem key={`${i}`}>
+                  <Message msg={ msg } getAllMsgs={ getAllMsgs } key={ `${msg}-${i}` }/>
+                </ListItem>
+              ) : (
+                <ListItem key={`${i}`} ref={ latestMsgRef }>
                   <Message msg={ msg } getAllMsgs={ getAllMsgs } key={ `${msg}-${i}` }/>
                 </ListItem>
               )
             }
           })
         }
-
       </List>
     </Box>
   );
